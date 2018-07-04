a6fdc1c7fa96b11395a37940be984decb5d88edf
Nikolay Skudnyakov <nikolay.skudnyakov@yotadevices.com>
2018-07-04 08:10:51 +0300

    View of plan with ability to add smart devices (just images so far)

--- web/smart-home/src/Components/BuildingPlan.js
+++ web/smart-home/src/Components/BuildingPlan.js
@@ -1,28 +1,27 @@
 import React, { Component } from 'react';
-import SmartItem from './SmartItem'
 import '../App.css'
 
 
 class BuildingPlan extends Component {
   
   constructor(prop) {	
-  	super(prop);
-  	console.log("building plan created " + prop);
+  	super(prop)
+  	console.log("building plan created " + prop)
+    this.onPlanClick = this.onPlanClick.bind(this)
   }
 
   onPlanClick(e) {
   	let x = e.nativeEvent.offsetX;
   	let y = e.nativeEvent.offsetY;
-  	// console.log(x);
-  	// console.log(y); 
-  	this.props.onAddSmartDevice(x,y);
+  	this.props.onClick({x,y});
   }
 
   render() {
     return (
-    	<img id="plan" src={this.props.imgSrc} alt="" onClick={(e) => this.onPlanClick(e)} />
+    	<img id="plan" src={this.props.imgSrc} alt="" onClick={e => this.onPlanClick(e)} />
     	);
   }
+
 }
 
 export default BuildingPlan;
