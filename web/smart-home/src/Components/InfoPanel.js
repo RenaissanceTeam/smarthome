ce275a2a126f9ff44573659ea50dca3e90d89945
Nikolay Skudnyakov <nikolay.skudnyakov@yotadevices.com>
2018-07-04 09:36:34 +0300

    Info panel to edit new smart devices.

---
+++ web/smart-home/src/Components/InfoPanel.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import '../App.css'
+import PropertyBlock from './PropertyBlock'
+import Constants from '../Constants'
+
+
+class InfoPanel extends Component {
+  render() {
+  	console.log("Rendering infoPanel");
+  	if (! this.props.info) {
+  		return null;
+  	} 
+
+  	if (this.props.info.type === "device") {
+  		let device = this.props.info
+  		return (
+  			<div id="infoPanel">
+	    	<PropertyBlock type={Constants.HEADER} title={device.title} />
+	    	{ device.props.map( ({key, title, type}) => <PropertyBlock key={key} type={type} title = {title} /> ) }
+
+
+	    	<button className="cancelButton" onClick={() => this.props.onCancelClicked()}> CANCEL </button>
+	    	<button className="okButton" onClick={() => this.props.onOkClicked() }> OK </button>
+    	</div>
+  			)
+  	}
+    return (
+    	<div id="infoPanel">
+	    	<PropertyBlock type={Constants.HEADER} title="Add items" />
+	    	<PropertyBlock type={Constants.INPUT_BLOCK} title="Some prop" />
+	    	<PropertyBlock type={Constants.CHECKABLE} title="Some prop" />
+
+	    	<input type="submit"/>
+    	</div>
+    	)
+	}
+}
+
+export default InfoPanel;
