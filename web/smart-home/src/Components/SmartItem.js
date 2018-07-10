547722ba23a634217a98f5d799e6c583291f02b9
Nikolay Skudnyakov <nikolay.skudnyakov@yotadevices.com>
2018-07-10 09:31:38 +0300

    Fixed right click (removed it)

--- web/smart-home/src/Components/SmartItem.js
+++ web/smart-home/src/Components/SmartItem.js
@@ -15,7 +15,7 @@ class SmartItem extends Component {
   render() {
     return ( 
     		<img draggable="true" className="smartItem" src={this.props.imgSrc} alt="" style = {{ top: this.props.y, left: this.props.x }} 
-    		    onClick={e => this.props.onClick(this.props.x, this.props.y)} onContextMenu={e => this.props.onRightClick(e)} 
+    		    onClick={e => this.props.onClick(this.props.x, this.props.y)} 
     		    />
     	);
   }
