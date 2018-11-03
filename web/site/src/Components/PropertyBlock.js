d95c90433be98da945293bb1679099093e5da938
Nikolay Skudnyakov <nikolay.skudnyakov@yotadevices.com>
2018-11-03 22:23:27 +0300

    web hosting

---
+++ web/site/src/Components/PropertyBlock.js
@@ -0,0 +1,169 @@
+import React, { Component } from 'react';
+import '../App.css'
+import plus from '../res/img/plus.png'
+import Constants from '../Constants'
+
+var shift = 1
+var generateKey = (pre) => `${ pre }_${ new Date().getTime() + shift++ }`;
+function myFunction(module, target) {
+
+    let moduleContainer = target.parentElement.parentElement
+
+    let y = target.offsetTop + target.parentElement.offsetTop + moduleContainer.offsetTop
+    let x =  target.offsetLeft + target.parentElement.offsetLeft + moduleContainer.offsetLeft
+
+    let selectOptions = document.getElementById("myDropdown")
+    selectOptions.style.top = y + "px"
+    selectOptions.style.left = x + "px"
+    console.log(selectOptions);
+    selectOptions.classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+class PropertyBlock extends Component {
+  constructor(props) {
+    super(props)
+    this.onOptionSelected = this.onOptionSelected.bind(this)
+    this.index = -1;
+
+  }
+
+  onOptionSelected(value ) {
+    console.log("selected " + value +" for index " + this.index);
+
+    let currentState = this.props.value;
+    currentState[this.index] = this.props.options.filter(o => o.title === value)[0]
+    this.props.onChange(currentState)
+  }
+
+  render() {
+    if (this.props.type === Constants.HEADER) {
+      return ( 
+          <div className="propertyBlock"> {this.props.value} </div>
+      	);
+    } 
+
+    if (this.props.type === Constants.DEVICE_TITLE) {
+      return <div 
+                className="propertyBlock deviceTitle" 
+                contentEditable="true" 
+                onInput={e => this.props.onChange(e.nativeEvent.target.textContent) }
+              > {this.props.value}  </div>
+    }
+
+    if (this.props.type === Constants.INPUT_BLOCK) {
+      return ( 
+          <div className="propertyBlock"> 
+            
+            <div className="propertyTitle"> {this.props.title}  </div>
+            <div className = "inputField"> 
+              <textArea rows="5" type="text" onChange={(e) => this.props.onChange(e.target.value)}> {this.props.value} </textArea> 
+            </div>
+           </div>
+        );
+    }
+
+    if (this.props.type === Constants.CHECKABLE) {
+      return ( 
+          <div className="propertyBlock"> 
+            
+            <div className="propertyTitle"> {this.props.title}  </div>
+            <div className = "inputField squaredThree"> 
+              <input type="checkbox" value="None" id="squaredThree" name="check"
+                   onChange={e => this.props.onChange(e.target.checked)} defaultChecked={this.props.value} /> 
+              <label for="squaredThree"></label>
+            </div>
+           </div>
+      );
+    }
+
+    if (this.props.type === Constants.MODULES_BLOCK) {
+      let value = this.props.value
+      if (!value) {
+        value = []
+      }
+      return  (
+        <div className = "propertyBlock modulesBlock">
+          <div > {this.props.title} </div>
+
+          <div className="dropdown">
+            <div id="myDropdown" className="dropdown-content" >
+              {
+               this.props.options.map(
+                  (module) => 
+                      <a key={generateKey("dropdown")} onClick={e => this.onOptionSelected(e.nativeEvent.target.textContent)} >{module.title}</a>
+                  )
+              }
+            </div>
+          </div>
+
+          { 
+            value.map( (module, index) => 
+              <div key={generateKey("module")} className="moduleBlock">
+                <div className="moduleBlockImageContainer" >
+                  <div 
+                      className="editButton dropbtn" 
+                      onClick={e => {
+                          myFunction(module, e.nativeEvent.target); this.index = index;
+                          }}>
+                        e
+                  </div>
+                  <img alt="" className="moduleBlockImage" src={module.img}/>
+                </div>
+
+                <div>{module.title}</div>
+              </div>
+          )}
+
+
+          <div className="moduleBlock">
+            <img alt="" className="moduleBlockImage" 
+                  src={plus}
+                  onClick={e => this.props.onChange([...this.props.value, {title: "new module", img: plus} ])}/>
+          </div>
+
+        </div>
+        )
+    }
+    if (this.props.type === Constants.OPTIONS) {
+      let index = 0
+
+      return (
+        <div className="propertyBlock">
+          <div className="propertyTitle"> {this.props.title} </div>
+          <div className="inputField">
+            <select id="soflow-color" onChange={e => this.props.onChange(e.target.value)} defaultValue={this.props.value} > 
+            {
+              this.props.options.map( option => 
+                <option key={index++} value= {option.title} > {option.title} </option>
+              )
+            }
+            </select>
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <div className="propertyBlock"> Unknown block </div>
+    );
+  
+  }
+}
+
+export default PropertyBlock;
---
+++ web/site/src/Components/SmartDevices.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react';
+import SmartItem from './SmartItem'
+import '../App.css'
+
+const visibleClassName = "infoWindowVisible"
+const invisibleClassName = "infoWindowInvisible"
+class SmartDevices extends Component {	
+	
+  constructor(props) {
+  	super(props)
+  	this.onHover = this.onHover.bind(this)
+  	this.hideInfoWindow = this.hideInfoWindow.bind(this)
+  	this.showInfoWindow = this.showInfoWindow.bind(this)
+  	this.isHovered = false
+  	this.state = {
+  		infoWindowClass: invisibleClassName,
+  		info: []
+  	}
+  }
+
+  
+  onHover(e, itemKey) {
+  	this.isHovered = true
+  	let device = this.props.devices.filter((device) => device.key === itemKey)[0]
+
+  	let x = e.target.offsetLeft + e.target.width
+  	let y = e.target.offsetTop + e.target.height
+  	setTimeout( () => {
+  		if (this.isHovered)  {
+  			let deviceTitle = device.infoState.filter( i => i.title === "Device Title" )[0].value
+  			let deviceDescription = device.infoState.filter( i => i.title === "Description" )[0].value
+  			this.showInfoWindow(x, y, {title: deviceTitle, description: deviceDescription})}
+		}
+  		, 1000)
+  }
+
+  hideInfoWindow() {
+  	this.isHovered = false
+  	// console.log("hide info window");
+  	this.setState({
+  		infoWindowClass: invisibleClassName,
+  		x: 0,
+  		y: 0,
+  	})
+  }
+
+  showInfoWindow(x, y, info) {
+  	// console.log("show info window at " + x + ", " + y + ", with info " + info);
+  	this.setState({
+  		infoWindowClass: visibleClassName,
+  		x: x,
+  		y: y,
+  		info: info
+  	})
+  }	
+
+
+	render() {
+	    return (
+	    	<div className="smartDevices"> 
+
+		    	<div >
+			    	{this.props.devices.map( item =>  
+			    		<SmartItem 
+			    			key={item.key} 
+			    			itemKey={item.key}
+			    			x={item.x} 
+			    			y={item.y} 
+			    			imgSrc={item.img}
+			    			onClick={this.props.onSmartDeviceClick}
+			    			onDragged={this.props.onDragged}
+			    			onHover={this.onHover}
+			    			hideInfoWindow={this.hideInfoWindow}
+			    			/>
+			    		)
+			    	}
+				</div>
+
+				<div className={this.state.infoWindowClass} id="smartItemInfoWindow" style = {{ top: this.state.y, left: this.state.x }}>
+					<h3> {this.state.info.title} </h3>
+					<p> {this.state.info.description} </p>
+				</div>
+	    	</div>
+	    )
+	}
+}
+
+export default SmartDevices;
