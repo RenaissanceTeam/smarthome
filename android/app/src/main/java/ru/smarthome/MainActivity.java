f9160e830204268ad182f48e5c91851cf842bc03
Makar <zlysenko.makarz@gmail.com>
2018-11-25 14:06:08 +0300

    auth with google

--- android/app/src/main/java/ru/smarthome/MainActivity.java
+++ android/app/src/main/java/ru/smarthome/MainActivity.java
@@ -1,7 +1,20 @@
 package ru.smarthome;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Collections;
+import java.util.List;
+
+import ru.smarthome.constants.Constants;
+
+import static ru.smarthome.constants.Constants.RC_SIGN_IN;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<AuthUI.IdpConfig> providers = Collections.singletonList(
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        startActivityForResult(AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (resultCode == RESULT_OK) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+            } else {
+                // TODO: guide user to the xuy
+            }
+        }
     }
 }
