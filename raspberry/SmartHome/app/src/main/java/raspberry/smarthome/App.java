37edb65bd724748e8b0a120a200623fcc6458d55
Makar <zlysenko.makarz@gmail.com>
2018-12-09 00:11:18 +0300

    app start tasks

---
+++ raspberry/SmartHome/app/src/main/java/raspberry/smarthome/App.java
@@ -0,0 +1,19 @@
+package raspberry.smarthome;
+
+import android.app.Application;
+import android.util.Log;
+
+import raspberry.smarthome.tasks.IpObtainTask;
+
+import static raspberry.smarthome.BuildConfig.DEBUG;
+
+public class App extends Application {
+
+    // execute smth once per app start
+    public App() {
+        if(DEBUG) Log.d(getClass().getName(), "App started");
+
+        new IpObtainTask().execute();
+    }
+
+}
