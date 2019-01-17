c4479390efed5832999dfcc7c87f2607baf05e65
Makar <zlysenko.makarz@gmail.com>
2019-01-17 15:05:29 +0300

    client datalibrary dependency

--- android_client/android/app/src/main/java/ru/smarthome/MainActivity.kt
+++ android_client/android/app/src/main/java/ru/smarthome/MainActivity.kt
@@ -2,20 +2,10 @@ package ru.smarthome
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import android.util.Log
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.ViewModelProviders
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
-import com.google.gson.GsonBuilder
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import ru.smarthome.constants.Constants.RASPBERRY_URL
 import ru.smarthome.constants.Constants.RC_SIGN_IN
 
 class MainActivity : FragmentActivity() {
@@ -30,6 +20,29 @@ class MainActivity : FragmentActivity() {
                 .commit()
 //        auth()
 //        requestHomeStateFromRaspberry() // todo after auth successful
+    }
+
+    private fun auth() {
+        val providers = listOf(AuthUI.IdpConfig.GoogleBuilder().build())
+
+        startActivityForResult(AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(), RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == RC_SIGN_IN) {
+            val response = IdpResponse.fromResultIntent(data)
+
+            if (resultCode == RESULT_OK) {
+                val user = FirebaseAuth.getInstance().currentUser
+            } else {
+
+            }
+        }
     }
 
 }
