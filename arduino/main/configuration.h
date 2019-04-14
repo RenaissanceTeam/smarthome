80b4bf3203c749bc76836e1f09abaa3f960276ea
Nikolay Skudnyakov <nikolay.skudnyakov@yotadevices.com>
2019-04-14 12:23:58 +0300

    Arduino configuration

--- arduino/main/configuration.h
+++ arduino/main/configuration.h
@@ -1,48 +1,46 @@
 #include "user_properties.h"
 #include "services.h"
 
-#define DEVICE_NAME "arduino1"
+#define DEVICE_NAME "arduinonano"
 #define RASPBERRY_PORT 8080
 #define UDP_PORT       59743
 #define ARDUINO_PORT   8080
 
 // used services
+#define INIT_SERVICE
 #define TEMPERATURE_DHT22
+#define DIGITAL_ALERT
 #define HUMIDITY_DHT22
-#define TEMPERATURE_DHT11
-#define HUMIDITY_DHT11
 #define ONOFF
-#define DIGITAL_ALERT
-#define INIT_SERVICE 
-#define DIGITAL_ALERT
-
-
-// pins for services
-#define onoff_1 2
-#define onoff_2 3
-#define alert_pin 4
-#define DHT22_PIN 5
-#define INIT_PIN 8
-#define DHT11_PIN 9
-
-
+#define ANALOG
 
 // pins for communication with esp8266
-#define RX 6 // attach to TX of esp
-#define TX 7 // attach to RX of esp
+#define RX 2 // attach to TX of esp
+#define TX 3 // attach to RX of esp
 
 // static services array, pay attention to the defined pins
-int SERVICES[] = {TEMPERATURE_DHT11_ID, HUMIDITY_DHT11_ID, ONOFF_ID, ONOFF_ID, DIGITAL_ALERT_ID, INIT_SERVICE_ID};
-int PINS[] = {DHT11_PIN, DHT11_PIN, onoff_1, onoff_2, alert_pin, INIT_PIN};
-#define SERVICES_STR "1002;1003;1001;1001;1004;1005"
-#define SERVICES_COUNT 6
-#define DIGITAL_ALERT_COUNT 1
-
-// will be called on arduino 'setup()''
+int SERVICES[] = {ANALOG_ID, ANALOG_ID, ANALOG_ID, ANALOG_ID, ANALOG_ID, ANALOG_ID, DIGITAL_ALERT_ID, DIGITAL_ALERT_ID, DIGITAL_ALERT_ID, DIGITAL_ALERT_ID, TEMPERATURE_DHT22_ID, HUMIDITY_DHT22_ID, INIT_SERVICE_ID, ONOFF_ID, ONOFF_ID, ONOFF_ID};
+int PINS[] = {A0, A1, A2, A3, A4, A5, 4, 5, 6, 7, 8, 8, 9, 10, 11, 12};
+#define SERVICES_STR "1000;1000;1000;1000;1000;1000;1004;1004;1004;1004;1006;1007;1005;1001;1001;1001" 
+#define SERVICES_COUNT 16
+#define DIGITAL_ALERT_COUNT 4
+#define INIT_PIN 9
+// will be called on arduino 'setup()'
 void setupConfiguration() {
-	pinMode(onoff_1, OUTPUT);
-	pinMode(onoff_2, OUTPUT);
-	pinMode(alert_pin, INPUT);
-  pinMode(DHT11_PIN, INPUT);
-	pinMode(INIT_PIN, INPUT);
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+  pinMode(4, INPUT);
+  pinMode(5, INPUT);
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
 }
