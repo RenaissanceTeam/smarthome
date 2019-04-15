056a747a39eefb09e9c435d97ca28d28c0ff2d26
makar <zlysenko.makarz@gmail.com>
2019-04-16 01:26:41 +0300

    make all controllers readable

--- raspberry/SmartHome/thirdpartydevices/src/main/java/smarthome/raspberry/thirdpartydevices/xiaomi/yeelight/controller/FlowController.kt
+++ raspberry/SmartHome/thirdpartydevices/src/main/java/smarthome/raspberry/thirdpartydevices/xiaomi/yeelight/controller/FlowController.kt
@@ -1,14 +1,19 @@
 package smarthome.raspberry.thirdpartydevices.xiaomi.yeelight.controller
 
+import smarthome.raspberry.thirdpartydevices.xiaomi.READ_NOT_SUPPORTED_MESSAGE
 import smarthome.raspberry.thirdpartydevices.xiaomi.yeelight.YeelightDevice
 import smarthome.raspberry.thirdpartydevices.xiaomi.yeelight.constants.FLOW_CONTROLLER_TYPE
+import smarthome.raspberry.thirdpartydevices.xiaomi.yeelight.controller.interfaces.YeelightReadable
 import smarthome.raspberry.thirdpartydevices.xiaomi.yeelight.controller.interfaces.YeelightWritable
 import smarthome.raspberry.thirdpartydevices.xiaomi.yeelight.result.Result
 
-class FlowController(device: YeelightDevice) : Controller(device, FLOW_CONTROLLER_TYPE), YeelightWritable {
+class FlowController(device: YeelightDevice) : Controller(device, FLOW_CONTROLLER_TYPE), YeelightWritable, YeelightReadable {
 
     override fun write(params: String): Result {
         throw Exception("not implemented")
     }
 
+    override fun read(): String {
+        return READ_NOT_SUPPORTED_MESSAGE
+    }
 }
