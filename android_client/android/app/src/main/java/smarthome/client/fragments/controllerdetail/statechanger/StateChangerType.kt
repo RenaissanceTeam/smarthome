ad0149f6e660bbb14650b5b129f99ce813f17263
makar <zlysenko.makarz@gmail.com>
2019-04-18 23:28:35 +0300

    controllers ui

--- android_client/android/app/src/main/java/smarthome/client/fragments/controllerdetail/statechanger/StateChangerType.kt
+++ android_client/android/app/src/main/java/smarthome/client/fragments/controllerdetail/statechanger/StateChangerType.kt
@@ -2,5 +2,11 @@ package smarthome.client.fragments.controllerdetail.statechanger
 
 enum class StateChangerType {
     ONOFF,
+    LEXEME_ONOFF,
+    SIMPLE_WRITE,
+    DIMMER,
+    GATEWAY_DIMMER,
+    GATEWAY_RGB,
+    TEXT_READ_WRITE,
     ONLY_READ // default, assume that each controller can be read
 }
