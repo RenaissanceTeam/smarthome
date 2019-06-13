98f5f0b0f1edd7e69ffcac6ab48ff59baafe71f8
Nikolay Skudniakov <nikolay.skudniakov@hiq.ru>
2019-06-13 12:45:10 +0300

    Setup bottom navigation

--- android_client/android/app/src/main/java/smarthome/client/viewpager/addition/AdditionPageFactory.kt
+++
@@ -1,8 +0,0 @@
-package smarthome.client.viewpager.addition
-
-import androidx.fragment.app.Fragment
-import smarthome.client.viewpager.PageFactory
-
-class AdditionPageFactory : PageFactory{
-    override fun createPageFragment(): Fragment = AdditionFragment()
-}
