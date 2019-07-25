85683479596b22d7cfb273cb8e85cbbcfe506946
Nikolay Skudniakov <nikolay.skudniakov@hiq.ru>
2019-07-25 13:58:12 +0300

    Make code in app module compile, upgrade dependencies versions

--- raspberry/SmartHome/app/src/main/java/smarthome/raspberry/presentation/MainActivity.kt
+++ raspberry/SmartHome/app/src/main/java/smarthome/raspberry/presentation/MainActivity.kt
@@ -10,31 +10,18 @@ import smarthome.raspberry.BuildConfig
 import smarthome.raspberry.R
 
 
-import smarthome.raspberry.presentation.auth.GoogleSignInActivity
 import smarthome.raspberry.domain.usecases.AuthUseCase
 import smarthome.raspberry.domain.usecases.DevicesUseCase
 import smarthome.raspberry.domain.usecases.HomeUseCase
-import smarthome.raspberry.data.legacy.model.SmartHomeRepository
-import smarthome.raspberry.data.legacy.model.listeners.RepoInitListener
-import smarthome.raspberry.arduinodevices.server.UdpServer
-import smarthome.raspberry.arduinodevices.server.WebServer
-import smarthome.raspberry.data.legacy.service.DeviceObserver
 
 private val TAG = MainActivity::class.java.simpleName
 private val DEBUG = BuildConfig.DEBUG
 private val AUTH_ACTIVITY_REQUEST_CODE = 12312
 
-class MainActivity : Activity(), RepoInitListener {
-    private var httpServer: WebServer? = null
-    private var udpServer: UdpServer? = null
+class MainActivity : Activity() {
     private val authUseCase: AuthUseCase = TODO()
     private val devicesUseCase: DevicesUseCase = TODO()
     private val homeUseCase: HomeUseCase = TODO()
-
-
-    val deviceObserver: DeviceObserver = DeviceObserver.getInstance()
-
-
     private val job = Job()
     private val uiScope = CoroutineScope(Dispatchers.Main + job)
 
@@ -43,14 +30,6 @@ class MainActivity : Activity(), RepoInitListener {
         setContentView(R.layout.activity_main)
         if (DEBUG) Log.d(TAG, "onCreate")
 
-        httpServer = WebServer()
-        udpServer = UdpServer()
-    }
-
-    override fun onStart() {
-        super.onStart()
-        httpServer?.startServer()
-        udpServer?.startServer()
     }
 
     override fun onResume() {
@@ -63,20 +42,8 @@ class MainActivity : Activity(), RepoInitListener {
         }
     }
 
-    override fun onStop() {
-        super.onStop()
-        httpServer?.stopServer()
-        udpServer?.stopServer()
-    }
-
     override fun onDestroy() {
         super.onDestroy()
         job.cancel()
-    }
-
-    override suspend fun onInitializationComplete() {
-        SmartHomeRepository.listenForCloudChanges()
-        SmartHomeRepository.subscribeToMessageQueue()
-        deviceObserver.start()
     }
 }
