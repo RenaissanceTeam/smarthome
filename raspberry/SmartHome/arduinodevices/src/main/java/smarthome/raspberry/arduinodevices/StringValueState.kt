b2cb7a7bc7f2733ffe8ea807eb8efaaf3aa21144
Nikolay Skudniakov <nikolay.skudniakov@hiq.ru>
2019-07-30 16:47:26 +0300

    Refactoring to string value controller state

---
+++ raspberry/SmartHome/arduinodevices/src/main/java/smarthome/raspberry/arduinodevices/StringValueState.kt
@@ -0,0 +1,5 @@
+package smarthome.raspberry.arduinodevices
+
+import smarthome.library.common.ControllerState
+
+class StringValueState(val value: String): ControllerState()
--- raspberry/SmartHome/arduinodevices/src/test/java/smarthome/raspberry/arduinodevices/server/WebServerTest.kt
+++ raspberry/SmartHome/arduinodevices/src/test/java/smarthome/raspberry/arduinodevices/server/WebServerTest.kt
@@ -6,33 +6,48 @@ import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import fi.iki.elonen.NanoHTTPD
 import kotlinx.coroutines.runBlocking
+import org.junit.Before
 import org.junit.Test
 import smarthome.library.common.BaseController
 import smarthome.library.common.DeviceChannelOutput
+import smarthome.raspberry.arduinodevices.StringValueState
 
 class WebServerTest {
 
     private val session: NanoHTTPD.IHTTPSession = mock()
     private val deviceChannelOutput: DeviceChannelOutput = mock()
     private val handler = WebHandler(deviceChannelOutput)
+    private val controller = BaseController("")
+
+    @Before
+    fun setUp() {
+        whenever(session.method).thenReturn(NanoHTTPD.Method.POST)
+        whenever(session.uri).thenReturn("/alert")
+        whenever(session.parms).thenReturn(mapOf(
+                "controller_guid" to "123",
+                "value" to "11.1"
+
+        ))
+        runBlocking {
+            whenever(deviceChannelOutput.findController(any())).thenReturn(controller)
+        }
+
+    }
 
     @org.junit.Test
     fun handleAlertShouldTriggerOutputBoundary() {
 
         runBlocking {
-            val controller = BaseController("")
-            whenever(session.method).thenReturn(NanoHTTPD.Method.POST)
-            whenever(session.uri).thenReturn("/alert")
-            whenever(session.parms).thenReturn(mapOf(
-                    "controller_guid" to "123"
-            ))
-            whenever(deviceChannelOutput.findController(any())).thenReturn(controller)
-
             handler.handle(session)
-
             verify(deviceChannelOutput).onNewState(controller)
         }
     }
 
-  
+    @Test
+    fun handleAlertProvidesValidState() {
+        runBlocking {
+            handler.handle(session)
+            assert((controller.state as StringValueState).value == "11.1")
+        }
+    }
 }
