107a893952ee3da3f3304b3202fadb99e05092f2
Nikolay Skudniakov <nikolay.skudniakov@hiq.ru>
2019-08-08 14:52:31 +0300

    Implement finding device and controllers in repo

--- raspberry/SmartHome/app/src/test/java/smarthome/raspberry/data/HomeRepositoryImplTest.kt
+++ raspberry/SmartHome/app/src/test/java/smarthome/raspberry/data/HomeRepositoryImplTest.kt
@@ -3,11 +3,15 @@ package smarthome.raspberry.data
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
+import io.kotlintest.shouldBe
+import io.kotlintest.shouldThrow
 import kotlinx.coroutines.runBlocking
 import org.junit.Test
 import org.mockito.Mockito.verify
 import smarthome.library.common.*
 import smarthome.raspberry.domain.HomeRepository
+import smarthome.raspberry.domain.NoControllerException
+import smarthome.raspberry.domain.NoDeviceException
 
 abstract class A(name: String, description: String?,
                  serveState: DeviceServeState, guid: Long,
@@ -30,6 +34,7 @@ class HomeRepositoryImplTest {
     private val device_A: A = mock()
     private val device_B: B = mock()
     private val controller: BaseController = mock()
+
 
 
     @Test
@@ -76,6 +81,91 @@ class HomeRepositoryImplTest {
             whenever(localStorage.findDevice(any())).then { device_A }
             repo.proceedWriteController(controller, state)
             verify(deviceChannel_A).writeState(device_A, controller, state)
+        }
+    }
+
+    @Test
+    fun noDevices_findController_shouldThrow() {
+        runBlocking {
+            val repo: DeviceChannelOutput =
+                    HomeRepositoryImpl(localStorage, remoteStorage, mapOf(
+                            device_A.javaClass to deviceChannel_A
+                    ))
+
+            whenever(localStorage.getDevices()).thenReturn(mutableListOf())
+
+            shouldThrow<NoControllerException> {
+                repo.findController(123)
+            }
+        }
+    }
+
+    @Test
+    fun hasDevicesWithProperController_findController_shouldReturnController() {
+        runBlocking {
+            val repo: DeviceChannelOutput =
+                    HomeRepositoryImpl(localStorage, remoteStorage, mapOf(
+                            device_A.javaClass to deviceChannel_A
+                    ))
+
+            val guid = 123L
+            val notProperController = mock<BaseController>()
+            whenever(device_A.controllers).then { listOf(controller) }
+            whenever(device_B.controllers).then { listOf(notProperController) }
+
+            whenever(controller.guid).then { guid }
+            whenever(notProperController.guid).then { guid + 1 }
+
+
+            whenever(localStorage.getDevices()).thenReturn(mutableListOf(
+                    device_B,
+                    device_A
+
+            ))
+
+            repo.findController(guid).shouldBe(controller)
+        }
+    }
+
+    @Test
+    fun noDevices_findDevice_shouldThrow() {
+        runBlocking {
+            val repo: DeviceChannelOutput =
+                    HomeRepositoryImpl(localStorage, remoteStorage, mapOf(
+                            device_A.javaClass to deviceChannel_A
+                    ))
+
+            whenever(localStorage.getDevices()).thenReturn(mutableListOf())
+
+            shouldThrow<NoDeviceException> {
+                repo.findDevice(controller)
+            }
+        }
+    }
+
+    @Test
+    fun hasDevicesWithProperController_findDevice_ShouldReturnDevice() {
+        runBlocking {
+            val repo: DeviceChannelOutput =
+                    HomeRepositoryImpl(localStorage, remoteStorage, mapOf(
+                            device_A.javaClass to deviceChannel_A
+                    ))
+
+            val guid = 123L
+            val notProperController = mock<BaseController>()
+            whenever(device_A.controllers).then { listOf(controller) }
+            whenever(device_B.controllers).then { listOf(notProperController) }
+
+            whenever(controller.guid).then { guid }
+            whenever(notProperController.guid).then { guid + 1 }
+
+            whenever(localStorage.getDevices()).thenReturn(mutableListOf(
+                    device_B,
+                    device_A
+
+            ))
+
+            repo.findDevice(controller).shouldBe(device_A)
         }
     }
 }
