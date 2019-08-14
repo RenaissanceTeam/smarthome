ae9abea23b18ae9dd4f235c6eff972faf740cc18
Nikolay Skudniakov <nikolay.skudniakov@hiq.ru>
2019-08-14 13:50:37 +0300

    Create local devices storage to hold devices info

--- raspberry/SmartHome/app/src/main/java/smarthome/raspberry/data/local/LocalDevicesStorage.kt
+++ raspberry/SmartHome/app/src/main/java/smarthome/raspberry/data/local/LocalDevicesStorage.kt
@@ -5,4 +5,9 @@ import smarthome.library.common.IotDevice
 interface LocalDevicesStorage {
     fun saveDevices(devices: List<IotDevice>)
     fun getSavedDevices(): List<IotDevice>
+    fun updateDevice(device: IotDevice)
+    fun add(device: IotDevice)
+    fun addPending(device: IotDevice)
+    fun removePending(device: IotDevice)
+    fun remove(device: IotDevice)
 }
--- raspberry/SmartHome/app/src/main/java/smarthome/raspberry/data/local/devices/LocalDevicesStorageImpl.kt
+++ raspberry/SmartHome/app/src/main/java/smarthome/raspberry/data/local/devices/LocalDevicesStorageImpl.kt
@@ -6,20 +6,45 @@ import com.google.gson.TypeAdapterFactory
 import smarthome.library.common.IotDevice
 import smarthome.raspberry.data.local.LocalDevicesStorage
 
-
-
-
 class LocalDevicesStorageImpl(typeAdapterFactory: TypeAdapterFactory) :
         LocalDevicesStorage {
-    var savedData: String? = null
     private val gson = GsonBuilder().registerTypeAdapterFactory(typeAdapterFactory).create()
     private val devicesType = object : TypeToken<ArrayList<IotDevice>>() {}.type
+    private var savedDevicesData: String? = null
+    private var savedPendingDevicesData: String? = null
+    private val devices: MutableList<IotDevice> = initializeSavedDevices()
+    private val pendingDevices: MutableList<IotDevice> = mutableListOf()
+
+    private fun initializeSavedDevices(): MutableList<IotDevice> {
+        return gson.fromJson(savedDevicesData, devicesType) ?: mutableListOf()
+    }
 
     override fun saveDevices(devices: List<IotDevice>) {
-        savedData = gson.toJson(devices, devicesType)
+        savedDevicesData = gson.toJson(devices, devicesType)
     }
 
     override fun getSavedDevices(): List<IotDevice> {
-        return gson.fromJson(savedData, devicesType) ?: listOf()
+        return devices
+    }
+
+    override fun updateDevice(device: IotDevice) {
+        devices[devices.indexOf(device)] = device
+    }
+
+    override fun add(device: IotDevice) {
+        devices.add(device)
+        saveDevices(devices)
+    }
+
+    override fun addPending(device: IotDevice) {
+        pendingDevices.add(device)
+    }
+
+    override fun removePending(device: IotDevice) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun remove(device: IotDevice) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
