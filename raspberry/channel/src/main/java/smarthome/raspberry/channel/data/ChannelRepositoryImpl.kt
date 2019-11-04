b205fa69ef2f3ca70e43bb4adb0e54772c149e6d
Nikolay Skudniakov <helloma@yandex.ru>
2019-11-04 21:56:11 +0300

    Create module for channels, implement repos in controller and device

---
+++ raspberry/channel/src/main/java/smarthome/raspberry/channel/data/ChannelRepositoryImpl.kt
@@ -0,0 +1,10 @@
+package smarthome.raspberry.channel.data
+
+import smarthome.library.common.DeviceChannel
+import smarthome.raspberry.channel_api.data.ChannelRepository
+
+class ChannelRepositoryImpl() : ChannelRepository{
+    override fun getDeviceChannels(): Map<String, DeviceChannel> {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
--- raspberry/controllers/src/main/java/smarthome/raspberry/controllers/domain/ReadControllerUseCaseImpl.kt
+++ raspberry/controllers/src/main/java/smarthome/raspberry/controllers/domain/ReadControllerUseCaseImpl.kt
@@ -3,15 +3,20 @@ package smarthome.raspberry.controllers.domain
 import smarthome.library.common.BaseController
 import smarthome.library.common.ControllerServeState
 import smarthome.library.common.IotDevice
-import smarthome.raspberry.controllers_api.data.ControllersRepository
+import smarthome.raspberry.channel_api.domain.GetChannelForDeviceUseCase
 import smarthome.raspberry.controllers_api.domain.ReadControllerUseCase
 import smarthome.raspberry.devices_api.domain.DevicesService
 
-class ReadControllerUseCaseImpl(private val repository: ControllersRepository,
-                                private val devicesService: DevicesService): ReadControllerUseCase {
+class ReadControllerUseCaseImpl(
+        private val getChannelForDeviceUseCase: GetChannelForDeviceUseCase,
+        private val devicesService: DevicesService) : ReadControllerUseCase {
+
     override suspend fun execute(device: IotDevice, controller: BaseController) {
-            repository.proceedReadController(controller)
-            controller.serveState = ControllerServeState.IDLE
+        val channel = getChannelForDeviceUseCase.execute(device)
+        val newState = channel.read(device, controller)
+
+        controller.state = newState
+        controller.serveState = ControllerServeState.IDLE
 
         devicesService.saveDevice(device)
     }
