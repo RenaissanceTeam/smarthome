27282d218ba1141dbc5727935519dfa86335ec02
Nikolay Skudniakov <helloma@yandex.ru>
2019-11-13 22:25:09 +0300

    Fix devices and controllers tests

--- raspberry/devices/src/test/java/smarthome/raspberry/devices/data/storage/DevicesUseCaseTest.kt
+++ raspberry/devices/src/test/java/smarthome/raspberry/devices/data/storage/DevicesUseCaseTest.kt
@@ -1,45 +1,35 @@
-package smarthome.raspberry.domain.usecases
+package smarthome.raspberry.devices.data.storage
 
-import com.google.common.truth.Truth.assertThat
-import com.nhaarman.mockitokotlin2.argThat
+import com.nhaarman.mockitokotlin2.mock
 import com.nhaarman.mockitokotlin2.whenever
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
-import org.mockito.Mock
 import org.mockito.Mockito.verify
-import org.mockito.MockitoAnnotations
-import smarthome.library.common.BaseController
-import smarthome.library.common.ControllerServeState
-import smarthome.library.common.ControllerState
 import smarthome.library.common.IotDevice
-import smarthome.raspberry.domain.HomeRepository
+import smarthome.raspberry.devices.api.domain.DevicesService
+import smarthome.raspberry.devices.data.DevicesRepository
+import smarthome.raspberry.devices.domain.DevicesServiceImpl
 
-class DevicesUseCaseTest {
+class DevicesServiceImplTest {
 
-    @Mock
-    private lateinit var repo: HomeRepository
-    @Mock
+    private lateinit var repo: DevicesRepository
     private lateinit var device: IotDevice
-
-    private val devicesUseCase by lazy { DevicesUseCase(repo) }
-    private val idleController = createController(1)
-    private val idleDevice = createDevice(2, mutableListOf(idleController))
-
-    private val pendingController = createController(1,
-            serveState = ControllerServeState.PENDING_READ)
-    private val pendingDevice = createDevice(2, mutableListOf(pendingController))
+    private lateinit var devicesService: DevicesService
 
 
     @Before
     fun setUp() {
-        MockitoAnnotations.initMocks(this)
+        repo = mock {}
+        device = mock {}
+        devicesService = DevicesServiceImpl(repo)
     }
 
     @Test
     fun homeWithoutDevices_addNewDevice_AddDeviceAsPending() {
         runBlocking {
-            devicesUseCase.addNewDevice(device)
+            whenever(repo.getCurrentDevices()).thenReturn(listOf())
+            devicesService.addNewDevice(device)
 
             verify(repo).addPendingDevice(device)
         }
@@ -48,84 +38,10 @@ class DevicesUseCaseTest {
     @Test
     fun hasPendingDevice_acceptPendingDevice_RemoveFromPendingAndAddDevice() {
         runBlocking {
-            devicesUseCase.acceptPendingDevice(device)
+            devicesService.acceptPendingDevice(device)
 
             verify(repo).removePendingDevice(device)
             verify(repo).addDevice(device)
-        }
-    }
-
-    @Test
-    fun onUserRequest_readController_ProceedReadAndUpdateServeState() {
-        whenever(repo.getCurrentDevices()).thenReturn(mutableListOf(idleDevice))
-
-        runBlocking {
-            devicesUseCase.readController(pendingDevice, pendingController)
-
-            verify(repo).proceedReadController(pendingController)
-            verify(repo).saveDevice(pendingDevice)
-            assertThat(pendingController.serveState).isEqualTo(ControllerServeState.IDLE)
-        }
-    }
-
-    private fun createController(guid: Long,
-                                 name: String = "",
-                                 state: ControllerState? = null,
-                                 serveState: ControllerServeState = ControllerServeState.IDLE)
-            : BaseController {
-        return BaseController(name,
-                serveState = serveState,
-                guid = guid,
-                state = state)
-    }
-
-    private fun createDevice(guid: Long,
-                             controllers: MutableList<BaseController>,
-                             name: String = "", description:
-                             String = ""): IotDevice {
-        return IotDevice(name,
-                description,
-                controllers = controllers,
-                guid = guid
-        )
-
-    }
-
-    @Test
-    fun onUserRequest_writeNewState_ProceedWriteAndUpdateServeState() {
-        val newState = ControllerState()
-        pendingController.state = newState
-        pendingController.serveState = ControllerServeState.PENDING_WRITE
-
-        whenever(repo.getCurrentDevices()).thenReturn(mutableListOf(idleDevice))
-
-        runBlocking {
-            devicesUseCase.writeController(pendingDevice, pendingController, newState)
-
-            verify(repo).proceedWriteController(pendingController, newState)
-            verify(repo).saveDevice(pendingDevice)
-            assertThat(pendingController.serveState).isEqualTo(ControllerServeState.IDLE)
-        }
-    }
-
-    @Test
-    fun onUserRequest_readController_savesNewReadValue() {
-        whenever(repo.getCurrentDevices()).thenReturn(mutableListOf(idleDevice))
-        val stateBefore = ControllerState()
-        val stateAfter = ControllerState()
-
-        pendingController.state = stateBefore
-
-        runBlocking {
-            whenever(repo.proceedReadController(pendingController)).thenAnswer {
-                pendingController.state = stateAfter
-                return@thenAnswer pendingController
-            }
-
-            devicesUseCase.readController(pendingDevice, pendingController)
-            verify(repo).saveDevice(argThat {
-                controllers.first().state == stateAfter
-            })
         }
     }
 }
