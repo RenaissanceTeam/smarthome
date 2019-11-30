a08724c7a8448753c81a1ce958fe8e308ad36cf7
Nikolay Skudniakov <helloma@yandex.ru>
2019-11-30 09:51:45 +0300

    Removed di mock module, won't mock dependencies

--- raspberry/di_mocks/src/main/java/smarthome/raspberry/di/mocks/frameworkDependentClasses.kt
+++
@@ -1,9 +0,0 @@
-package smarthome.raspberry.di.mocks
-
-import com.google.firebase.auth.FirebaseAuth
-import com.nhaarman.mockitokotlin2.mock
-import org.koin.dsl.module
-
-val frameworkDependentClasses = module(override = true) {
-    single { mock<FirebaseAuth> { }}
-}
