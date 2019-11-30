aa942ba6c469aec87109426b1dee94839a6d4bca
Nikolay Skudniakov <helloma@yandex.ru>
2019-11-30 10:27:37 +0300

    Add datalibrary api module

--- raspberry/external/src/main/java/smarthome/raspberry/external/di/ExternalModule.kt
+++ raspberry/external/src/main/java/smarthome/raspberry/external/di/ExternalModule.kt
@@ -1,11 +1,20 @@
 package smarthome.raspberry.external.di
 
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import smarthome.library.common.SmartHomeStorage
+import smarthome.library.datalibrary.FirestoreSmartHomeStorage
 import smarthome.raspberry.external.datalibrary.DatalibraryAdapter
 
 val externalModule = listOf(
         module {
-            factory { DatalibraryAdapter.provideSmartHomeStorage() }
+            
+            scope(named("homeId")) {
+                scoped<SmartHomeStorage> {
+                    FirestoreSmartHomeStorage(get(named("homeId")))
+                }
+            }
+            
             factory { DatalibraryAdapter.provideHomesReferencesStorage()}
         }
 )
