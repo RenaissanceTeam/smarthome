a99c0042ff2e5832ab825f82f5efe1e602841f95
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-01 21:22:24 +0300

    code cleanup

--- raspberry/home/src/main/java/smarthome/raspberry/home/data/storage/LocalStorageImpl.kt
+++ raspberry/home/src/main/java/smarthome/raspberry/home/data/storage/LocalStorageImpl.kt
@@ -4,11 +4,12 @@ import smarthome.raspberry.util.SharedPreferencesHelper
 
 class LocalStorageImpl(private val preferences: SharedPreferencesHelper):
     LocalStorage {
-    companion object {
-        private const val HOME_ID = "HOME_ID"
-    }
-    
+
     override suspend fun saveHome(homeId: String) {
         preferences.setString(HOME_ID, homeId)
+    }
+    
+    companion object {
+        private const val HOME_ID = "HOME_ID"
     }
 }
