e1f41673a097133774bb2cd91a026b96b8739f1d
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-07 22:15:04 +0300

    Code cleanup

--- raspberry/util/src/main/java/smarthome/raspberry/util/KoinExtensions.kt
+++
@@ -1,7 +0,0 @@
-package smarthome.raspberry.util
-
-import org.koin.core.module.Module
-
-fun List<Module>.addMockIfIsInMockMode(isMock: Boolean, mockModule: () -> Module): List<Module> {
-    return if (isMock) this + mockModule() else this
-}
