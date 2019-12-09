76d6cba2557a98205f34e059baaf59257b966310
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-09 15:30:04 +0300

    Add test for observing shared preferences changes

--- raspberry/util/src/main/java/smarthome/raspberry/util/SharedPreferencesHelper.kt
+++ raspberry/util/src/main/java/smarthome/raspberry/util/SharedPreferencesHelper.kt
@@ -4,12 +4,15 @@ import android.annotation.SuppressLint
 import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
+import io.reactivex.Observable
+import io.reactivex.subjects.PublishSubject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import kotlin.reflect.KClass
 
 
 class SharedPreferencesHelper(context: Context) {
-
+    private val observables = mutableMapOf<String, PublishSubject<Any>>()
     private val sharedPreferences: SharedPreferences by lazy {
         context.getSharedPreferences("smarthome", MODE_PRIVATE)
     }
@@ -23,5 +26,13 @@ class SharedPreferencesHelper(context: Context) {
 
     fun getString(key: String, default: String = ""): String {
         return sharedPreferences.getString(key, default) ?: default
+    }
+    
+    fun <T> observe(key: String): Observable<T> {
+        val observable = observables[key] ?:
+    }
+    
+    private fun <T> createNewObservable(key: String): PublishSubject<T> {
+        return PublishSubject.create<T>().apply { observables[key] = this }
     }
 }
--- raspberry/util/src/test/java/smarthome/raspberry/util/SharedPreferencesHelperTest.kt
+++ raspberry/util/src/test/java/smarthome/raspberry/util/SharedPreferencesHelperTest.kt
@@ -1,5 +1,42 @@
 package smarthome.raspberry.util
 
-class SharedPreferencesHelperTest {
+import android.content.Context
+import android.content.SharedPreferences
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
 
+class SharedPreferencesHelperTest {
+    
+    private lateinit var context: Context
+    private lateinit var mockPrefs: SharedPreferences
+    private lateinit var sharedPreferencesHelper: SharedPreferencesHelper
+    
+    @Before
+    fun setUp() {
+        mockPrefs = mock {}
+        context = mock {
+            on { getSharedPreferences(any(), any()) }.thenReturn(mockPrefs)
+        }
+        sharedPreferencesHelper = SharedPreferencesHelper(context)
+    }
+    
+    @Test
+    fun `when set new value for key should emit value for observable`() {
+        val first = "first"
+        val second = "second"
+        
+        val result = sharedPreferencesHelper.observe<String>("key").test()
+        
+        runBlocking {
+            sharedPreferencesHelper.setString("key", first)
+            sharedPreferencesHelper.setString("key", second)
+        }
+        
+        result
+            .assertValueAt(0, first)
+            .assertValueAt(1, second)
+    }
 }
