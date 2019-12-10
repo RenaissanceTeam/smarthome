21266e0f4da6fcbd7498996f952a635c2c6aa9bc
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-10 14:05:14 +0300

    Implement observable preference

--- raspberry/util/src/main/java/smarthome/raspberry/util/persistence/preferences/Preference.kt
+++ raspberry/util/src/main/java/smarthome/raspberry/util/persistence/preferences/Preference.kt
@@ -1,11 +1,9 @@
 package smarthome.raspberry.util.persistence.preferences
 
-import smarthome.raspberry.util.persistence.PersistentStorage
 import kotlin.reflect.KClass
 
-class Preference<T : Any>(val key: String,
-                                  val ofType: KClass<T>,
-                                  val persistentStorage: PersistentStorage) {
-    fun get() = persistentStorage.get(key, ofType)
-    suspend fun set(value: T) = persistentStorage.set(key, value)
+interface Preference<T: Any> {
+    val ofType: KClass<T>
+    fun get(): T
+    suspend fun set(value: T)
 }
