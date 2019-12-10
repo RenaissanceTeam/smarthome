752cc5f08efe2d4e270d381e85e2470ee6e5f2c5
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-10 10:54:39 +0300

    Implement preferences helper to set and get values to  preference object

--- raspberry/util/src/main/java/smarthome/raspberry/util/SharedPreferencesHelper.kt
+++ raspberry/util/src/main/java/smarthome/raspberry/util/SharedPreferencesHelper.kt
@@ -5,14 +5,14 @@ import android.content.Context
 import android.content.Context.MODE_PRIVATE
 import android.content.SharedPreferences
 import io.reactivex.Observable
-import io.reactivex.subjects.PublishSubject
+import io.reactivex.subjects.BehaviorSubject
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import kotlin.reflect.KClass
 
 
 class SharedPreferencesHelper(context: Context) {
-    private val observables = mutableMapOf<String, PublishSubject<Any>>()
+    private val preferences = mutableMapOf<String, Preference<*>>()
     private val sharedPreferences: SharedPreferences by lazy {
         context.getSharedPreferences("smarthome", MODE_PRIVATE)
     }
@@ -24,15 +24,56 @@ class SharedPreferencesHelper(context: Context) {
         }
     }
 
+    fun <T : Any> set(key: String, value: T) {
+        preferences[key] = Preference(key, value)
+    }
+    
+    inline fun <reified T: Any> get(key: String) = get(key, T::class)
+    
+    fun <T : Any> get(key: String, expectedType: KClass<out T>): T {
+        val preference = preferences[key] ?: throw IllegalArgumentException(
+            "can't read key=$key as it is not stored")
+        val saved = preference.get()
+        
+        try {
+            return expectedType.java.cast(saved)
+                ?: throw IllegalStateException("value for key=$key is null")
+        } catch (e: ClassCastException) {
+            throw IllegalArgumentException(
+                "can'r read key=$key as saved type (${saved::class}) doesn't match expected ($expectedType)")
+        }
+        
+    }
+    
     fun getString(key: String, default: String = ""): String {
         return sharedPreferences.getString(key, default) ?: default
     }
+
+//    fun <T> observe(key: String): Observable<T> {
+//        return observables[key] ?: createNewObservable<T>(key)
+//    }
+//
+//    private fun <T> createNewObservable(key: String): PublishSubject<T> {
+//        return PublishSubject.create<T>().apply { observables[key] = this }
+//    }
+}
+
+private class Preference<T : Any>(key: String, default: T) {
+    private var value = default
     
-    fun <T> observe(key: String): Observable<T> {
-        val observable = observables[key] ?:
+    fun get() = value
+    
+    fun set(value: T) {
+        this.value = value
+    }
+}
+
+private class ObservablePreference<T>(key: String) {
+    private val subject = BehaviorSubject.create<T>()
+    
+    fun observe(): Observable<T> {
+        return subject
     }
     
-    private fun <T> createNewObservable(key: String): PublishSubject<T> {
-        return PublishSubject.create<T>().apply { observables[key] = this }
-    }
+    fun getValue(default: T): T = subject.value ?: default
 }
--- raspberry/util/src/test/java/smarthome/raspberry/util/SharedPreferencesHelperTest.kt
+++ raspberry/util/src/test/java/smarthome/raspberry/util/SharedPreferencesHelperTest.kt
@@ -2,11 +2,13 @@ package smarthome.raspberry.util
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.google.common.truth.Truth.assertThat
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.mock
 import kotlinx.coroutines.runBlocking
 import org.junit.Before
 import org.junit.Test
+import kotlin.test.assertFailsWith
 
 class SharedPreferencesHelperTest {
     
@@ -23,20 +25,51 @@ class SharedPreferencesHelperTest {
         sharedPreferencesHelper = SharedPreferencesHelper(context)
     }
     
+//    @Test
+//    fun `when set new value for key should emit value for observable`() {
+//        val first = "first"
+//        val second = "second"
+//
+//        val result = sharedPreferencesHelper.observe<String>("key").test()
+//
+//        runBlocking {
+//            sharedPreferencesHelper.setString("key", first)
+//            sharedPreferencesHelper.setString("key", second)
+//        }
+//
+//        result
+//            .assertValueAt(0, first)
+//            .assertValueAt(1, second)
+//    }
+    
     @Test
-    fun `when set new value for key should emit value for observable`() {
-        val first = "first"
-        val second = "second"
+    fun `when set new value to preference it should hold it and return on read`() {
+        val value = "a"
+        val key = "key"
+        sharedPreferencesHelper.set(key, value)
         
-        val result = sharedPreferencesHelper.observe<String>("key").test()
+        val result = sharedPreferencesHelper.get<String>(key)
         
-        runBlocking {
-            sharedPreferencesHelper.setString("key", first)
-            sharedPreferencesHelper.setString("key", second)
-        }
-        
-        result
-            .assertValueAt(0, first)
-            .assertValueAt(1, second)
+        assertThat(result).isEqualTo(value)
     }
+    
+    @Test
+    fun `when trying to read preference that is not stored should throw`() {
+        assertFailsWith<IllegalArgumentException> {
+            sharedPreferencesHelper.get<String>("something")
+        }
+    }
+    
+    @Test
+    fun `when read preference of type other than expected should throw`() {
+        sharedPreferencesHelper.set("int", 1)
+        
+        assertFailsWith<IllegalArgumentException> {
+            val result = sharedPreferencesHelper.get<String>("int")
+        }
+    }
+    
+    
+    
+    
 }
