151edd6e4c73be20a62d3b46d0d82a5fe0271d8d
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-10 14:20:14 +0300

    Clean up code

--- raspberry/util/src/main/java/smarthome/raspberry/util/persistence/preferences/PersistentPreference.kt
+++ raspberry/util/src/main/java/smarthome/raspberry/util/persistence/preferences/PersistentPreference.kt
@@ -3,9 +3,9 @@ package smarthome.raspberry.util.persistence.preferences
 import smarthome.raspberry.util.persistence.PersistentStorage
 import kotlin.reflect.KClass
 
-class PersistentPreference<T : Any>(val key: String,
+class PersistentPreference<T : Any>(private val key: String,
                                     override val ofType: KClass<T>,
-                                    val persistentStorage: PersistentStorage): Preference<T> {
+                                    private val persistentStorage: PersistentStorage): Preference<T> {
     override fun get() = persistentStorage.get(key, ofType)
     override suspend fun set(value: T) = persistentStorage.set(key, value)
 }
