5a6811f55c235bc60f69c6dfb2adb717dea0691c
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-11 09:43:29 +0300

    Add sign in command and entity-dto mappers

--- raspberry/authentication/src/main/java/smarthome/raspberry/authentication/presentation/AuthenticationPresenter.kt
+++ raspberry/authentication/src/main/java/smarthome/raspberry/authentication/presentation/AuthenticationPresenter.kt
@@ -2,11 +2,12 @@ package smarthome.raspberry.authentication.presentation
 
 import androidx.lifecycle.LifecycleObserver
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import smarthome.raspberry.authentication.api.domain.Credentials
 
 interface AuthenticationPresenter : LifecycleObserver {
     fun signIn()
     fun sighOut()
     fun deleteAll()
-    fun onAuthenticationSuccess(account: GoogleSignInAccount)
+    fun onAuthenticationSuccess(credentials: Credentials)
     fun onAuthenticationFail()
 }
---
+++ raspberry/authentication_api/src/main/java/smarthome/raspberry/authentication/api/domain/Credentials.kt
@@ -0,0 +1,3 @@
+package smarthome.raspberry.authentication.api.domain
+
+data class Credentials(val idToken: String)
