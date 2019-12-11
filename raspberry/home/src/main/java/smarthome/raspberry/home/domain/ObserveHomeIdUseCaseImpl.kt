cbe3074bda1bc4b5eace3966c10bcf3b4a02c18f
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-11 11:05:29 +0300

    Code cleanup

--- raspberry/home/src/main/java/smarthome/raspberry/home/domain/ObserveHomeIdUseCaseImpl.kt
+++ raspberry/home/src/main/java/smarthome/raspberry/home/domain/ObserveHomeIdUseCaseImpl.kt
@@ -3,13 +3,10 @@ package smarthome.raspberry.home.domain
 import io.reactivex.Observable
 import smarthome.raspberry.home.api.domain.ObserveHomeIdUseCase
 import smarthome.raspberry.home.data.HomeRepository
-import smarthome.raspberry.util.persistence.StorageHelper
-import smarthome.raspberry.util.persistence.observe
 
-class ObserveHomeIdUseCaseImpl(private val repository: HomeRepository): ObserveHomeIdUseCase {
-    override fun execute(): Observable<String> = repository.getHomeId()
+class ObserveHomeIdUseCaseImpl(
+    private val repository: HomeRepository
+) : ObserveHomeIdUseCase {
     
-    companion object {
-        internal const val HOME_ID = "HOME_ID"
-    }
+    override fun execute(): Observable<String> = repository.getHomeId()
 }
