d11df85858db24b2f79cbb44ff69b785a04b56f3
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-16 08:05:09 +0300

    Emit default value for observable prefs if set

--- raspberry/util/src/main/java/smarthome/raspberry/util/persistence/preferences/PersistentPreference.kt
+++ raspberry/util/src/main/java/smarthome/raspberry/util/persistence/preferences/PersistentPreference.kt
@@ -4,10 +4,10 @@ import smarthome.raspberry.util.persistence.NoStoredPreference
 import smarthome.raspberry.util.persistence.PersistentStorage
 import kotlin.reflect.KClass
 
-class PersistentPreference<T : Any>(private val key: String,
+class PersistentPreference<T : Any>(override val key: String,
                                     override val ofType: KClass<T>,
                                     private val persistentStorage: PersistentStorage): Preference<T> {
-    private var default: T? = null
+    override var default: T? = null
     
     override fun get(): T {
         return try {
@@ -16,10 +16,7 @@ class PersistentPreference<T : Any>(private val key: String,
             default ?: throw e
         }
     }
-    override suspend fun set(value: T) = persistentStorage.set(key, value)
     
-    override fun setDefault(value: T) {
-        default = value
-    }
+    override suspend fun set(value: T) = persistentStorage.set(key, value)
 }
 
--- raspberry/util/src/main/java/smarthome/raspberry/util/persistence/preferences/Preference.kt
+++ raspberry/util/src/main/java/smarthome/raspberry/util/persistence/preferences/Preference.kt
@@ -4,7 +4,8 @@ import kotlin.reflect.KClass
 
 interface Preference<T: Any> {
     val ofType: KClass<T>
+    val key: String
+    var default: T?
     fun get(): T
     suspend fun set(value: T)
-    fun setDefault(value: T)
 }
