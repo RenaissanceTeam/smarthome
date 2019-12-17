b6076f3ed54e24502933ef2c42ff2e4dd77cf858
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-17 12:01:15 +0300

    Implement alert request

--- raspberry/arduinodevices/src/main/java/smarthome/raspberry/arduinodevices/data/server/httphandlers/AlertPost.kt
+++ raspberry/arduinodevices/src/main/java/smarthome/raspberry/arduinodevices/data/server/httphandlers/AlertPost.kt
@@ -1,38 +1,38 @@
 package smarthome.raspberry.arduinodevices.data.server.httphandlers
 
-import smarthome.library.common.BaseController
-import smarthome.library.common.ControllerState
+import smarthome.library.common.Id
 import smarthome.raspberry.arduinodevices.data.server.api.RequestHandler
-import smarthome.raspberry.arduinodevices.data.server.entity.Method
-import smarthome.raspberry.arduinodevices.data.server.entity.RequestIdentifier
-import smarthome.raspberry.arduinodevices.data.server.entity.Response
-import smarthome.raspberry.arduinodevices.domain.state.StringValueState
+import smarthome.raspberry.arduinodevices.data.server.entity.*
+import smarthome.raspberry.arduinodevices.data.server.mapper.ValuePayloadToControllerStateMapper
+import smarthome.raspberry.controllers.api.domain.GetControllerByIdUseCase
+import smarthome.raspberry.controllers.api.domain.OnControllerChangedWithoutUserRequestUseCase
 
-class AlertPost: RequestHandler {
-    override val identifier = RequestIdentifier(Method.POST, "/iot/alert")
+class AlertPost(
+    private val getControllerByIdUseCase: GetControllerByIdUseCase,
+    private val onControllerChangedWithoutUserRequestUseCase: OnControllerChangedWithoutUserRequestUseCase,
+    private val valueMapper: ValuePayloadToControllerStateMapper
+) : RequestHandler {
+    override val identifier = RequestIdentifier(Method.POST, "/iot/alert", parameters = setOf(
+        controllerId, value
+    ))
     
     override suspend fun serve(parameters: Map<String, String>): Response {
-        TODO()
+        return try {
+            val id = parameters[controllerId]?.takeIf { it.isNotEmpty() } ?: throw BadParams()
+            val value = parameters[value] ?: throw BadParams()
+            
+            val controller = getControllerByIdUseCase.execute(Id(id))
+            val newState = valueMapper.map(value)
+                
+            onControllerChangedWithoutUserRequestUseCase.execute(controller, newState)
+            
+            success()
+        } catch (e: NumberFormatException) {
+            badRequest("passed bad controller id. params=$parameters")
+        } catch (e: BadParams) {
+            badRequest("bad params=$parameters")
+        }
+        
+        
     }
-    
-    //    private lateinit var value: ControllerState
-//    private lateinit var controller: BaseController
-
-//    override suspend fun serve(session: NanoHTTPD.IHTTPSession): NanoHTTPD.Response {
-//        return try {
-//            parseRequest(session)
-//            controller.state = value
-//            output.onNewState(controller)
-//            NanoHTTPD.Response("alert ok")
-//        } catch (e: Exception) {
-//            NanoHTTPD.Response("alert exception $e")
-//            throw e
-//        }
-//    }
-//
-//    private suspend fun parseRequest(session: NanoHTTPD.IHTTPSession) {
-//        val params = session.parms
-//        value = StringValueState(params["value"] ?: throw IllegalArgumentException("no value"))
-//        controller = getController(params)
-//    }
 }
---
+++ raspberry/arduinodevices/src/test/java/smarthome/raspberry/arduinodevices/data/server/httphandlers/AlertPostTest.kt
@@ -0,0 +1,67 @@
+package smarthome.raspberry.arduinodevices.data.server.httphandlers
+
+import com.google.common.truth.Truth.assertThat
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.whenever
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import smarthome.library.common.BaseController
+import smarthome.library.common.ControllerState
+import smarthome.library.common.Id
+import smarthome.raspberry.arduinodevices.data.server.entity.BAD_REQUEST_CODE
+import smarthome.raspberry.arduinodevices.data.server.entity.SUCCESS_CODE
+import smarthome.raspberry.arduinodevices.data.server.mapper.ValuePayloadToControllerStateMapper
+import smarthome.raspberry.controllers.api.domain.GetControllerByIdUseCase
+import smarthome.raspberry.controllers.api.domain.OnControllerChangedWithoutUserRequestUseCase
+
+class AlertPostTest {
+    
+    private lateinit var alertPost: AlertPost
+    private lateinit var onControllerChangedUC: OnControllerChangedWithoutUserRequestUseCase
+    private lateinit var getControllerByIdUseCase: GetControllerByIdUseCase
+    private lateinit var valueMapper: ValuePayloadToControllerStateMapper
+    
+    @Before
+    fun setUp() {
+        onControllerChangedUC = mock {}
+        getControllerByIdUseCase = mock {}
+        valueMapper = mock {}
+        alertPost = AlertPost(getControllerByIdUseCase, onControllerChangedUC, valueMapper)
+    }
+    
+    
+    @Test
+    fun `when improper guid passed should return bad request`() {
+        val result = runBlocking {
+            alertPost.serve(mapOf(controllerId to "", value to "value"))
+        }
+        
+        assertThat(result.code).isEqualTo(BAD_REQUEST_CODE)
+    }
+    
+    @Test
+    fun `should return success code when everything ok`() {
+        val result = runBlocking {
+            alertPost.serve(mapOf(controllerId to "1", value to "value"))
+        }
+        
+        assertThat(result.code).isEqualTo(SUCCESS_CODE)
+    }
+    
+    @Test
+    fun `when serve() should trigger OnControllerChanged use case`() {
+        runBlocking {
+            val controller = mock<BaseController>()
+            val state = mock<ControllerState>()
+            
+            whenever(getControllerByIdUseCase.execute(Id(("1")))).then { controller }
+            whenever(valueMapper.map("value")).then { state }
+    
+            alertPost.serve(mapOf(controllerId to "1", value to "value"))
+            
+            verify(onControllerChangedUC).execute(controller, state)
+        }
+    }
+}
