859c848b59e648738c8b2e74c33521e62e1c4ca8
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-25 12:11:54 +0300

    Inject handlers properly

--- raspberry/arduinodevices/src/main/java/smarthome/raspberry/arduinodevices/data/server/di/ServerModule.kt
+++ raspberry/arduinodevices/src/main/java/smarthome/raspberry/arduinodevices/data/server/di/ServerModule.kt
@@ -1,12 +1,18 @@
 package smarthome.raspberry.arduinodevices.data.server.di
 
+import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import org.koin.experimental.builder.factoryBy
 import org.koin.experimental.builder.singleBy
 import smarthome.raspberry.arduinodevices.data.server.UdpServer
 import smarthome.raspberry.arduinodevices.data.server.WebServerImpl
+import smarthome.raspberry.arduinodevices.data.server.api.RequestHandler
 import smarthome.raspberry.arduinodevices.data.server.api.WebServer
 import smarthome.raspberry.arduinodevices.data.server.api.WebServerGate
+import smarthome.raspberry.arduinodevices.data.server.httphandlers.AlertPost
+import smarthome.raspberry.arduinodevices.data.server.httphandlers.InitPost
+import smarthome.raspberry.arduinodevices.data.server.httphandlers.alert
+import smarthome.raspberry.arduinodevices.data.server.httphandlers.init
 import smarthome.raspberry.arduinodevices.data.server.mapper.*
 import smarthome.raspberry.arduinodevices.data.server.nano.DelegatableNanoHttpd
 import smarthome.raspberry.arduinodevices.data.server.nano.NanoHttpdToWebServerAdapter
@@ -23,4 +29,7 @@ val serverModule = module {
     factoryBy<HttpSessionToRequestMapper, HttpSessionToRequestMapperImpl>()
     factoryBy<ResponseCodeToStatusMapper, ResponseCodeToStatusMapperImpl>()
     factoryBy<ResponseToNanoResponseMapper, ResponseToNanoResponseMapperImpl>()
+    
+    factoryBy<RequestHandler, InitPost>(named(init))
+    factoryBy<RequestHandler, AlertPost>(named(alert))
 }
