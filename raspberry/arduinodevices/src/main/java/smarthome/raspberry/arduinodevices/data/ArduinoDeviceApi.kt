1edcbca6cd4805b4e9ecc4450850281206f5762a
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-25 10:31:13 +0300

    Fix tests

--- raspberry/arduinodevices/src/main/java/smarthome/raspberry/arduinodevices/data/ArduinoDeviceApi.kt
+++ raspberry/arduinodevices/src/main/java/smarthome/raspberry/arduinodevices/data/ArduinoDeviceApi.kt
@@ -3,16 +3,17 @@ package smarthome.raspberry.arduinodevices.data
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Query
+import smarthome.library.common.Id
 import smarthome.raspberry.arduinodevices.data.dto.ArduinoControllerResponse
 
 interface ArduinoDeviceApi {
 
     @GET("service")
-    suspend fun controllerReadRequest(@Query("index") serviceIndex: Int): ArduinoControllerResponse
+    suspend fun controllerReadRequest(@Query("id") controllerId: Id): ArduinoControllerResponse
 
     @POST("service")
     suspend fun controllerWriteRequest(
-            @Query("index") serviceIndex: Int,
+            @Query("id") controllerId: Id,
             @Query("value") value: String): ArduinoControllerResponse
 
 }
