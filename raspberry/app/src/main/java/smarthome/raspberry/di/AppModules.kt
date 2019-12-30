3b9f8a10646f534582285c15ff4d667b7e3d3b9d
Nikolay Skudniakov <helloma@yandex.ru>
2019-12-30 13:35:59 +0300

    Uncomment koin code

--- raspberry/app/src/main/java/smarthome/raspberry/di/AppModules.kt
+++ raspberry/app/src/main/java/smarthome/raspberry/di/AppModules.kt
@@ -1,26 +1,26 @@
-//package smarthome.raspberry.di
-//
-//import smarthome.raspberry.arduinodevices.di.arduinoModule
-//import smarthome.raspberry.authentication.di.authenticationModule
-//import smarthome.raspberry.channel.di.channelModule
-//import smarthome.raspberry.controllers.di.controllersModule
-//import smarthome.raspberry.devices.di.devicesModule
-//import smarthome.raspberry.external.di.externalModule
-//import smarthome.raspberry.home.di.homeModule
-//import smarthome.raspberry.input.di.inputModule
-//import smarthome.raspberry.notification.di.notificationModule
-//import smarthome.raspberry.scripts.di.scriptsModule
-//import smarthome.raspberry.util.di.utilModule
-//
-//val appModules = authenticationModule +
-//                 channelModule +
-//                 controllersModule +
-//                 devicesModule +
-//                 homeModule +
-//                 inputModule +
-//                 notificationModule +
-//                 scriptsModule +
-//                 utilModule +
-//                 externalModule +
-//                 arduinoModule
-//
+package smarthome.raspberry.di
+
+import smarthome.raspberry.arduinodevices.di.arduinoModule
+import smarthome.raspberry.authentication.di.authenticationModule
+import smarthome.raspberry.channel.di.channelModule
+import smarthome.raspberry.controllers.di.controllersModule
+import smarthome.raspberry.devices.di.devicesModule
+import smarthome.raspberry.external.di.externalModule
+import smarthome.raspberry.home.di.homeModule
+import smarthome.raspberry.input.di.inputModule
+import smarthome.raspberry.notification.di.notificationModule
+import smarthome.raspberry.scripts.di.scriptsModule
+import smarthome.raspberry.util.di.utilModule
+
+val appModules = authenticationModule +
+                 channelModule +
+                 controllersModule +
+                 devicesModule +
+                 homeModule +
+                 inputModule +
+                 notificationModule +
+                 scriptsModule +
+                 utilModule +
+                 externalModule +
+                 arduinoModule
+
