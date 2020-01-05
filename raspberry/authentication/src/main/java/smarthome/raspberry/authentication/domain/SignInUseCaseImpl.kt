e71bf43d78c2a48b15cb2a8ba11045f692d19f45
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-05 07:00:03 +0300

    Refactoring to remove dependency from spring in domain layer

--- raspberry/authentication/src/main/java/smarthome/raspberry/authentication/domain/SignInUseCaseImpl.kt
+++ raspberry/authentication/src/main/java/smarthome/raspberry/authentication/domain/SignInUseCaseImpl.kt
@@ -1,29 +1,22 @@
 package smarthome.raspberry.authentication.domain
 
-import org.springframework.security.authentication.AuthenticationManager
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
-import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.stereotype.Component
 import smarthome.raspberry.authentication.api.domain.SignInUseCase
 import smarthome.raspberry.authentication.api.domain.dto.TokenResponse
 import smarthome.raspberry.authentication.api.domain.entity.Credentials
+import smarthome.raspberry.authentication.data.AuthenticatorAdapter
 import smarthome.raspberry.authentication.data.jwt.JwtTokenProvider
 
 @Component
 class SignInUseCaseImpl(
-    private val authenticationManager: AuthenticationManager,
+    private val authenticatorAdapter: AuthenticatorAdapter,
     private val tokenProvider: JwtTokenProvider
 ): SignInUseCase {
     
     override fun execute(credentials: Credentials): TokenResponse {
-        val authentication = authenticationManager.authenticate(UsernamePasswordAuthenticationToken(
-            credentials.login,
-            credentials.password
-        ))
+        authenticatorAdapter.authenticate(credentials)
         
-        SecurityContextHolder.getContext().authentication = authentication
-        
-        val token = tokenProvider.generateToken(authentication)
+        val token = tokenProvider.generateToken(credentials.login)
         return TokenResponse(token)
     }
 }
