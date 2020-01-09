a8435640806b89d84d10a246a80cd000f6f803ac
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-10 02:00:25 +0300

    Fix view visibility bug

--- android_client/presentation/src/main/java/smarthome/client/presentation/Extensions.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/Extensions.kt
@@ -5,7 +5,7 @@ import android.view.View
 var View.visible
     get() = visibility == View.VISIBLE
     set(value: Boolean) {
-    visibility = when (visible) {
+    visibility = when (value) {
         true -> View.VISIBLE
         false -> View.GONE
     }
