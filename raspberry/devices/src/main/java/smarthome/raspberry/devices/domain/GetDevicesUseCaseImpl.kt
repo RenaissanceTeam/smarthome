1a87cb56598563b62968944b2ed24267f2d326e6
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-10 04:53:18 +0300

    Put code that access data layer into domain

--- raspberry/devices/src/main/java/smarthome/raspberry/devices/domain/GetDevicesUseCaseImpl.kt
+++ raspberry/devices/src/main/java/smarthome/raspberry/devices/domain/GetDevicesUseCaseImpl.kt
@@ -1,8 +1,14 @@
 package smarthome.raspberry.devices.domain
 
+import org.springframework.stereotype.Component
 import smarthome.raspberry.devices.api.domain.GetDevicesUseCase
 import smarthome.raspberry.devices.data.DevicesRepository
+import smarthome.raspberry.devices.data.mapper.DeviceToGeneralDeviceInfoMapper
+import smarthome.raspberry.entity.Device
 
-class GetDevicesUseCaseImpl(private val repository: DevicesRepository): GetDevicesUseCase {
-    override suspend fun execute() = TODO()
+@Component
+open class GetDevicesUseCaseImpl(
+        private val repository: DevicesRepository
+) : GetDevicesUseCase {
+    override fun execute(): List<Device> = repository.findAll()
 }
