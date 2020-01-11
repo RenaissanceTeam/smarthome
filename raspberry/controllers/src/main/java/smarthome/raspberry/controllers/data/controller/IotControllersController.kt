529f6ca211fff702f0feeb69cac0eaabdb84a737
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-12 01:42:00 +0300

    Implement read controller use case

--- raspberry/controllers/src/main/java/smarthome/raspberry/controllers/data/controller/IotControllersController.kt
+++ raspberry/controllers/src/main/java/smarthome/raspberry/controllers/data/controller/IotControllersController.kt
@@ -1,24 +1,33 @@
 package smarthome.raspberry.controllers.data.controller
 
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import smarthome.raspberry.controllers.api.data.dto.ControllerDetails
 import smarthome.raspberry.controllers.api.data.mapper.ControllerToControllerDetailsMapper
 import smarthome.raspberry.controllers.api.domain.GetControllerByIdUseCase
+import smarthome.raspberry.controllers.api.domain.ReadControllerUseCase
+import smarthome.raspberry.entity.Controller
 import smarthome.raspberry.util.exceptions.notFound
 
 @RestController
 @RequestMapping("api/")
 class IotControllersController(
         private val getControllerByIdUseCase: GetControllerByIdUseCase,
+        private val readControllerUseCase: ReadControllerUseCase,
         private val controllerDetailsMapper: ControllerToControllerDetailsMapper
 ) {
 
     @GetMapping("controllers/{id}")
     fun getDetails(@PathVariable id: Long): ControllerDetails {
-        val controller = getControllerByIdUseCase.runCatching { execute(id) }.getOrElse { throw notFound }
+        val controller = getController(id)
         return controllerDetailsMapper.map(controller)
+    }
+
+    @PostMapping("controllers/{id}")
+    fun readState(@PathVariable id: Long): String {
+        return readControllerUseCase.execute(getController(id))
+    }
+
+    private fun getController(id: Long): Controller {
+        return getControllerByIdUseCase.runCatching { execute(id) }.getOrElse { throw notFound }
     }
 }
--- raspberry/controllers/src/main/java/smarthome/raspberry/controllers/domain/ReadControllerUseCaseImpl.kt
+++ raspberry/controllers/src/main/java/smarthome/raspberry/controllers/domain/ReadControllerUseCaseImpl.kt
@@ -1,25 +1,23 @@
 package smarthome.raspberry.controllers.domain
 
+import org.springframework.stereotype.Component
 import smarthome.raspberry.channel.api.domain.GetChannelForDeviceUseCase
 import smarthome.raspberry.controllers.api.domain.ReadControllerUseCase
+import smarthome.raspberry.controllers.data.ControllersRepo
 import smarthome.raspberry.devices.api.domain.SaveDeviceUseCase
 import smarthome.raspberry.entity.Controller
 
-class ReadControllerUseCaseImpl(
+@Component
+open class ReadControllerUseCaseImpl(
         private val getChannelForDeviceUseCase: GetChannelForDeviceUseCase,
-        private val saveDeviceUseCase: SaveDeviceUseCase) :
+        private val controllersRepo: ControllersRepo) :
         ReadControllerUseCase {
 
-    override suspend fun execute(controller: Controller) {
-        TODO()
-        
-        
-//        val channel = getChannelForDeviceUseCase.execute(device)
-//        val newState = channel.read(device, controller)
-//
-//        controller.state = newState
-//        controller.serveState = ControllerServeState.IDLE
-//
-//        saveDeviceUseCase.execute(device)
+    override fun execute(controller: Controller): String {
+        val channel = getChannelForDeviceUseCase.execute(controller.device)
+        val newState = channel.read(controller)
+
+        controllersRepo.save(controller.copy(state = newState))
+        return newState
     }
 }
