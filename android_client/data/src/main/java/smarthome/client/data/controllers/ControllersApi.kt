26ae72e29b3fb7f08c9d0e02e24776793c055fea
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-13 02:14:54 +0300

    Pass dto between client and server

--- android_client/data/src/main/java/smarthome/client/data/controllers/ControllersApi.kt
+++ android_client/data/src/main/java/smarthome/client/data/controllers/ControllersApi.kt
@@ -4,6 +4,7 @@ import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.Path
+import smarthome.client.data.controllers.dto.StateDto
 import smarthome.client.entity.Controller
 
 interface ControllersApi {
@@ -12,8 +13,8 @@ interface ControllersApi {
     suspend fun getDetails(@Path("id") id: Long): Controller
     
     @GET("api/controllers/{id}/read")
-    suspend fun readState(@Path("id") id: Long): String
+    suspend fun readState(@Path("id") id: Long): StateDto
     
     @POST("api/controllers/{id}/write")
-    suspend fun changeState(@Path("id") id: Long, @Body state: String): String
+    suspend fun changeState(@Path("id") id: Long, @Body state: StateDto): StateDto
 }
