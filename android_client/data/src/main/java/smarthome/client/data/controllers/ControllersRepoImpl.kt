932f7b5e5d52437d3ab159b1b765b9cd89fbc040
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-13 00:16:55 +0300

    Add last data param to datastatus, implement controller repo set state

--- android_client/data/src/main/java/smarthome/client/data/controllers/ControllersRepoImpl.kt
+++ android_client/data/src/main/java/smarthome/client/data/controllers/ControllersRepoImpl.kt
@@ -7,7 +7,6 @@ import smarthome.client.data.api.controllers.ControllersRepo
 import smarthome.client.data.retrofit.RetrofitFactory
 import smarthome.client.entity.Controller
 import smarthome.client.util.*
-import java.util.*
 
 class ControllersRepoImpl(
     private val retrofitFactory: RetrofitFactory
@@ -25,15 +24,52 @@ class ControllersRepoImpl(
     }
     
     override suspend fun setState(controllerId: Long, state: String): String {
-        delay(2000)
-        (0..2).random().takeIf { it == 1 }?.let { throw Throwable("random error") }
-        getOrCreateSubject(controllerId).apply {
-            val currentValue = value
-            if (currentValue is Data) {
-                onNext(Data(currentValue.data.copy(state = state)))
-            }
+        if (noController(controllerId)) {
+            get(controllerId)
         }
+        
+        emitLoading(controllerId)
+        retrofitFactory.createApi(ControllersApi::class.java)
+            .runCatching { changeState(controllerId, state) }
+            .onSuccess {
+                val lastController = getLastData(controllerId)?.data
+                    ?: throw Throwable("No info about controller when setState")
+                emitData(controllerId, lastController.copy(state = it))
+            }
+            .onFailure { emitError(controllerId, it) }
+            .getOrThrow()
+        
         return state
+    }
+    
+    private fun emitLoading(id: Long) {
+        val subject = getOrCreateSubject(id)
+        val lastData = getLastData(id)
+        
+        subject.onNext(LoadingStatus(lastData))
+    }
+    
+    private fun emitData(id: Long, controller: Controller) {
+        val subject = getOrCreateSubject(id)
+        subject.onNext(Data(controller))
+    }
+    
+    private fun emitError(id: Long, cause: Throwable) {
+        val subject = getOrCreateSubject(id)
+        val lastData = getLastData(id)
+    
+        subject.onNext(ErrorStatus(cause, lastData))
+    }
+    
+    private fun noController(id: Long) = getLastData(id) == null
+    
+    private fun getLastData(id: Long): Data<Controller>? {
+        return when (val value = getOrCreateSubject(id).value) {
+            is Data -> value
+            is ErrorStatus -> value.lastData
+            is LoadingStatus -> value.lastData
+            else -> null
+        }
     }
     
     override suspend fun readState(controllerId: Long): String {
@@ -50,13 +86,11 @@ class ControllersRepoImpl(
     }
     
     override suspend fun get(id: Long): Controller {
-        val subject = getOrCreateSubject(id)
-        
-        subject.onNext(LoadingStatus())
+        emitLoading(id)
         return retrofitFactory.createApi(ControllersApi::class.java)
             .runCatching { getDetails(id) }
-            .onSuccess { subject.onNext(Data(it)) }
-            .onFailure { subject.onNext(ErrorStatus(it)) }
+            .onSuccess { emitData(id, it) }
+            .onFailure { emitError(id, it) }
             .getOrThrow()
     }
 }
