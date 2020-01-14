2a4f3509c526b5a0161b5b3865c37f824a016dd1
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-14 14:36:10 +0300

    Code cleanup

--- android_client/presentation/src/main/java/smarthome/client/presentation/devices/deviceaddition/epoxy/PendingDeviceItemState.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/devices/deviceaddition/epoxy/PendingDeviceItemState.kt
@@ -1,8 +1,6 @@
 package smarthome.client.presentation.devices.deviceaddition.epoxy
 
 import smarthome.client.domain.api.devices.dto.GeneralDeviceInfo
-import smarthome.client.entity.Controller
-import smarthome.client.util.DataStatus
 
 data class PendingDeviceItemState (
     val device: GeneralDeviceInfo,
@@ -12,10 +10,3 @@ data class PendingDeviceItemState (
     val declineInProgress: Boolean
 )
 
-data class PendingControllerItemState(
-    val id: Long,
-    val name: String,
-    val type: String,
-    val state: String,
-    val isRefreshing: Boolean
-)
