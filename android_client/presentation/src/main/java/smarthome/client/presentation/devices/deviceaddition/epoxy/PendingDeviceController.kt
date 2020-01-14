69fd7b9d0b85e56d960d3a525fc4f2913da052d1
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-14 08:33:39 +0300

    Add pending controller views

--- android_client/presentation/src/main/java/smarthome/client/presentation/devices/deviceaddition/epoxy/PendingDeviceController.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/devices/deviceaddition/epoxy/PendingDeviceController.kt
@@ -12,6 +12,16 @@ class PendingDeviceController : Typed2EpoxyController<List<PendingDeviceItemStat
                 id(it.device.id)
                 device(it.device)
                 expanded(it.isExpanded)
+                controllers(it.device.controllers.map { controller ->
+                    PendingControllerViewModel_().apply {
+                        id(controller.id)
+                        name(controller.name)
+                        type(controller.type)
+                        state(controller.state)
+                        refreshing(it.controllerRefreshing[controller.id] ?: false)
+                        spanSizeOverride { total, _, _ -> total/2 }
+                    }
+                })
                 onExpand { viewModel.onExpand(it.device.id) }
                 onDeviceClick { viewModel.onDeviceClicked(it.device.id) }
             }
