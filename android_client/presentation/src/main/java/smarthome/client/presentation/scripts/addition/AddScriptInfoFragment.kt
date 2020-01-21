dcaac25fc5c7a3a689c2d1cae5536d4c8b87b286
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-21 21:28:38 +0300

    Share add scripts viewmodel between fragments

--- android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/AddScriptInfoFragment.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/AddScriptInfoFragment.kt
@@ -4,17 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.fragment_add_script.*
 import org.koin.android.ext.android.inject
+import org.koin.android.viewmodel.ext.android.sharedViewModel
 import smarthome.client.presentation.R
 import smarthome.client.presentation.main.toolbar.ToolbarController
-import smarthome.client.util.log
 
 class AddScriptInfoFragment : Fragment() {
-    private val viewModel: AddScriptViewModel by activity?.viewModels()
+    private val viewModel: AddScriptViewModel by sharedViewModel()
     private val toolbarController: ToolbarController by inject()
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +23,6 @@ class AddScriptInfoFragment : Fragment() {
     
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-    
-        log("view model in 1 = $viewModel")
     
         toolbarController.setMenu(R.menu.next) {
             if (it != R.id.next) return@setMenu
