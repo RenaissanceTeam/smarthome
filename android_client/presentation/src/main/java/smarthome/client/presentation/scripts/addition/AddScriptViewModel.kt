dca683afa28b4626329b0d19a4ca6901c701b016
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-21 22:01:51 +0300

    Add save menu, implement navigating back from add script flow

--- android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/AddScriptViewModel.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/AddScriptViewModel.kt
@@ -8,11 +8,17 @@ import smarthome.client.presentation.util.NavigationLiveData
 class AddScriptViewModel : KoinViewModel() {
     val scriptToAdd = MutableLiveData<Script>()
     val navigateToAddingController = NavigationLiveData()
+    val finishFlow = NavigationLiveData()
     
     fun onNextFromScriptInfoClicked(name: String, description: String) {
         val script = scriptToAdd.value ?: Script()
         scriptToAdd.value = script.copy(name = name, description = description)
         
         navigateToAddingController.trigger()
+    }
+    
+    fun onSaveClicked() {
+        // todo save to server
+        finishFlow.trigger()
     }
 }
