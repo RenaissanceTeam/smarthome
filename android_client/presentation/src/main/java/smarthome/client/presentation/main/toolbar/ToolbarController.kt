2b790d38fd69c1d731c647f5b3b86805fcd07d38
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-21 21:05:46 +0300

    Add second setup page for scripts

--- android_client/presentation/src/main/java/smarthome/client/presentation/main/toolbar/ToolbarController.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/main/toolbar/ToolbarController.kt
@@ -3,4 +3,5 @@ package smarthome.client.presentation.main.toolbar
 interface ToolbarController {
     fun setMenu(menuResource: Int, onMenuItemClick: (Int) -> Unit)
     fun clearMenu()
+    fun setTitle(value: String)
 }
--- android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/AddControllersToScriptFragment.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/AddControllersToScriptFragment.kt
@@ -6,41 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
-import androidx.navigation.fragment.findNavController
-import kotlinx.android.synthetic.main.fragment_add_script.*
+import androidx.lifecycle.observe
 import org.koin.android.ext.android.inject
 import smarthome.client.presentation.R
 import smarthome.client.presentation.main.toolbar.ToolbarController
 import smarthome.client.util.log
 
-class AddScriptInfoFragment : Fragment() {
+class AddControllersToScriptFragment : Fragment() {
     private val viewModel: AddScriptViewModel by activity?.viewModels()
     private val toolbarController: ToolbarController by inject()
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_add_script, container, false)
+        return inflater.inflate(R.layout.fragment_add_controllers_to_script, container, false)
     }
     
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-    
-        log("view model in 1 = $viewModel")
-    
-        toolbarController.setMenu(R.menu.next) {
-            if (it != R.id.next) return@setMenu
-            viewModel.onNextFromScriptInfoClicked(
-                script_name.text.toString(),
-                script_description.text.toString()
-            )
+        log("view model in 2 = $viewModel")
+        viewModel.scriptToAdd.observe(this) {
+            toolbarController.setTitle(it.name)
         }
-    
-        viewModel.navigateToAddingController.onNavigate(this, ::goToAddingControllers)
-    }
-    
-    private fun goToAddingControllers() {
-        findNavController().navigate(
-            AddScriptInfoFragmentDirections.actionAddScriptInfoFragmentToAddControllersToScriptFragment())
     }
 }
-
