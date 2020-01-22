acb33b15e327dd27a1cf5762c0bbe6de90c0a8c3
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-22 16:20:28 +0300

    Rename add controller fragment to script graph, add controllers view

--- android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/ScriptGraphFragment.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/ScriptGraphFragment.kt
@@ -1,19 +1,24 @@
-package smarthome.client.presentation.scripts.addition
+package smarthome.client.presentation.scripts.addition.graph
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.observe
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_add_controllers_to_script.*
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.sharedViewModel
 import smarthome.client.presentation.R
 import smarthome.client.presentation.main.toolbar.ToolbarController
+import smarthome.client.presentation.scripts.addition.AddScriptViewModel
+import smarthome.client.presentation.visible
 
-class AddControllersToScriptFragment : Fragment() {
-    private val viewModel: AddScriptViewModel by sharedViewModel()
+class ScriptGraphFragment : Fragment() {
+    private val addScriptViewModel: AddScriptViewModel by sharedViewModel()
+    private val graphViewModel: ScriptGraphViewModel by viewModels()
     private val toolbarController: ToolbarController by inject()
     
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -27,13 +32,22 @@ class AddControllersToScriptFragment : Fragment() {
         toolbarController.setMenu(R.menu.save) {
             if (it != R.id.save) return@setMenu
             
-            viewModel.onSaveClicked()
+            addScriptViewModel.onSaveClicked()
         }
         
-        viewModel.finishFlow.onNavigate(this, ::finishFlow)
+        addScriptViewModel.finishFlow.onNavigate(this, ::finishFlow)
         
-        viewModel.scriptToAdd.observe(this) {
+        addScriptViewModel.scriptToAdd.observe(this) {
             toolbarController.setTitle(it.name)
+        }
+    }
+    
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    
+        add_controller.setOnClickListener {
+            fab_menu.hideMenu(true)
+            add_controllers.visible = true
         }
     }
     
