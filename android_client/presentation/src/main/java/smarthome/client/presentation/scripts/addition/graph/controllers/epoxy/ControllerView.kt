18f4b3909c9645b04d805e23394edd8a4a12ec76
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-23 22:39:45 +0300

    Create list of controllers and devices in sliding menu

---
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/controllers/epoxy/ControllerView.kt
@@ -0,0 +1,31 @@
+package smarthome.client.presentation.scripts.addition.graph.controllers.epoxy
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.FrameLayout
+import com.airbnb.epoxy.*
+import kotlinx.android.synthetic.main.scripts_controller_item.view.*
+import smarthome.client.presentation.R
+import smarthome.client.presentation.util.inflate
+
+@ModelView(autoLayout = ModelView.Size.MATCH_WIDTH_WRAP_HEIGHT)
+class ControllerView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0
+) : FrameLayout(context, attrs, defStyleAttr) {
+    
+    init {
+        inflate(R.layout.scripts_controller_item)
+    }
+    
+    lateinit var name: CharSequence @TextProp set
+    lateinit var state: CharSequence @TextProp set
+    
+    @AfterPropsSet
+    fun onPropsReady() {
+        controller_name.text = name
+        controller_state.text = state
+    }
+    
+ }
---
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/controllers/epoxy/DeviceItemState.kt
@@ -0,0 +1,3 @@
+package smarthome.client.presentation.scripts.addition.graph.controllers.epoxy
+
+data class DeviceItemState(val id: Long, val name: String, val controllers: List<Long>)
