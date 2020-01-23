d0c809b3e32a6ede7cef2647c736ccc2c45d37c5
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-23 17:26:20 +0300

    Implement sliding to close and open

--- android_client/presentation/src/test/java/smarthome/client/presentation/scripts/addition/graph/controllers/ControllersViewViewModelTest.kt
+++ android_client/presentation/src/test/java/smarthome/client/presentation/scripts/addition/graph/controllers/ControllersViewViewModelTest.kt
@@ -22,14 +22,16 @@ class ControllersViewViewModelTest {
     
     @Test
     fun `when action down and move should emit delta`() {
-        viewModel.onActionDown(310f, 0f, 100f)
+        viewModel.setWidth(100f)
+        viewModel.onActionDown(310f, 0f)
         viewModel.moveTo(320f)
         assertThat(viewModel.dragging.value).isEqualTo(10f)
     }
     
     @Test
     fun `should not move to left more than where it started`() {
-        viewModel.onActionDown(310f, 0f, 100f)
+        viewModel.setWidth(100f)
+        viewModel.onActionDown(310f, 0f)
         viewModel.moveTo(309f)
     
         assertThat(viewModel.dragging.value).isEqualTo(0f)
@@ -37,7 +39,8 @@ class ControllersViewViewModelTest {
     
     @Test
     fun `moving right from out of bounds should be considered as moving from edge`() {
-        viewModel.onActionDown(310f, 0f, 100f)
+        viewModel.setWidth(100f)
+        viewModel.onActionDown(310f, 0f)
         viewModel.moveTo(200f)
         assertThat(viewModel.dragging.value).isEqualTo(0f)
 
@@ -47,7 +50,8 @@ class ControllersViewViewModelTest {
     
     @Test
     fun `moving left from out of bounds should be considered as moving from edge`() {
-        viewModel.onActionDown(310f, 0f, 100f)
+        viewModel.setWidth(100f)
+        viewModel.onActionDown(310f, 0f)
         viewModel.moveTo(450f)
         assertThat(viewModel.dragging.value).isEqualTo(100f)
     
@@ -57,12 +61,33 @@ class ControllersViewViewModelTest {
     
     @Test
     fun `moving should be recognized only if started on left side`() {
-        viewModel.onActionDown(310f, 0f, 100f)
+        viewModel.setWidth(100f)
+        viewModel.onActionDown(310f, 0f)
         viewModel.moveTo(320f)
         assertThat(viewModel.dragging.value).isEqualTo(10f)
     
-        viewModel.onActionDown(310f, 90f, 100f)
+        viewModel.onActionDown(310f, 90f)
         viewModel.moveTo(311f)
         assertThat(viewModel.dragging.value).isEqualTo(10f) // value not changed
+    }
+    
+    @Test
+    fun `when action up and progress less than half should animate to open`() {
+        viewModel.setWidth(100f)
+        viewModel.onActionDown(310f, 0f)
+        viewModel.moveTo(320f)
+        viewModel.onActionUp()
+        
+        assertThat(viewModel.animateTo.value).isEqualTo(0f)
+    }
+    
+    @Test
+    fun `when action up and progress more than half should animate to close`() {
+        viewModel.setWidth(100f)
+        viewModel.onActionDown(300f, 0f)
+        viewModel.moveTo(360f)
+        viewModel.onActionUp()
+    
+        assertThat(viewModel.animateTo.value).isEqualTo(100f)
     }
 }
