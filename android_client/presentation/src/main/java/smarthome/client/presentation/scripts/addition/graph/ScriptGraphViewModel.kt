f7d3478019943396da7962c7f341f0c8d35f5b84
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-28 15:11:48 +0300

    Small refactoring

--- android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/ScriptGraphViewModel.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/ScriptGraphViewModel.kt
@@ -6,32 +6,32 @@ import smarthome.client.presentation.util.KoinViewModel
 class ScriptGraphViewModel : KoinViewModel() {
     
     val blocks = MutableLiveData<MutableMap<GraphBlockIdentifier, GraphBlock>>(mutableMapOf())
-    val hiddenBlocks = MutableLiveData<MutableMap<GraphBlockIdentifier, Boolean>>()
+//    val hiddenBlocks = MutableLiveData<MutableMap<GraphBlockIdentifier, Boolean>>()
     
     fun onDropped(info: DragOperationInfo, dropPosition: Position) {
         val identifier = info.blockId
         val current = blocks.value ?: mutableMapOf()
         
         val addedBlock = current[identifier] ?: GraphBlock(id = identifier, type = info.blockType)
-        val blockOnNewPosition = addedBlock.copy(position = dropPosition) // todo add shift with draggable.touchPosition
+        val blockOnNewPosition = addedBlock.copy(position = dropPosition) // todo add shift with info.touchPosition
         
         current[identifier] = blockOnNewPosition
         blocks.value = current
     }
     
-    fun onDragStarted(id: GraphBlockIdentifier, dragTouch: Position): DragOperationInfo {
-        (hiddenBlocks.value ?: mutableMapOf())[id] = true
-        
-        triggerDevicesRebuildModels()
-        return DragOperationInfo("controllersHub", dragTouch, "controller",
-            ControllerGraphBlockIdentifier(id)) { droppedTo ->
-            when (droppedTo) {
-                "controllersHub" -> {
-                    onDragCancelled(id)
-                    true
-                }
-                else -> false
-            }
-        }
-    }
+//    fun onDragStarted(id: GraphBlockIdentifier, dragTouch: Position): DragOperationInfo {
+//        (hiddenBlocks.value ?: mutableMapOf())[id] = true
+//
+//        triggerDevicesRebuildModels()
+//        return DragOperationInfo("controllersHub", dragTouch, "controller",
+//            ControllerGraphBlockIdentifier(id)) { droppedTo ->
+//            when (droppedTo) {
+//                "controllersHub" -> {
+//                    onDragCancelled(id)
+//                    true
+//                }
+//                else -> false
+//            }
+//        }
+//    }
 }
