0ebc27bbb12f0c9eca6917a304817c786c2c5054
Nikolay Skudniakov <helloma@yandex.ru>
2020-01-30 13:15:14 +0300

    Draw controller info when it's added to graph

--- android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/views/GraphControllerView.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/views/GraphControllerView.kt
@@ -7,22 +7,16 @@ import android.view.MotionEvent
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.distinctUntilChanged
 import androidx.lifecycle.observe
-import org.koin.core.inject
+import kotlinx.android.synthetic.main.scripts_controller_item.view.*
+import smarthome.client.entity.Controller
 import smarthome.client.presentation.R
 import smarthome.client.presentation.scripts.addition.graph.GraphBlockView
 import smarthome.client.presentation.scripts.addition.graph.Position
-import smarthome.client.presentation.scripts.addition.graph.events.GraphEventBus
-import smarthome.client.presentation.scripts.addition.graph.events.drag.CommonDragInfo
-import smarthome.client.presentation.scripts.addition.graph.events.drag.ControllerDragEvent
-import smarthome.client.presentation.scripts.addition.graph.events.drag.DRAG_START
-import smarthome.client.presentation.scripts.addition.graph.events.drag.GRAPH
 import smarthome.client.presentation.scripts.addition.graph.views.state.ControllerBlock
 import smarthome.client.presentation.scripts.addition.graph.views.state.GraphBlock
 import smarthome.client.presentation.util.CustomDragShadowBuilder
-import smarthome.client.presentation.util.KoinViewModel
 import smarthome.client.presentation.util.inflate
 import smarthome.client.presentation.util.lifecycleOwner
 import smarthome.client.presentation.visible
@@ -59,15 +53,27 @@ class GraphControllerView @JvmOverloads constructor(
     }
     
     private fun observeViewModel(lifecycleOwner: LifecycleOwner) {
+        lifecycleOwner.lifecycle.addObserver(viewModel)
         
         viewModel.visible.distinctUntilChanged().observe(lifecycleOwner) { visible = it }
         viewModel.position.distinctUntilChanged().observe(lifecycleOwner, ::moveTo)
+        viewModel.data.observe(lifecycleOwner, ::bind)
+        viewModel.loading.distinctUntilChanged().observe(lifecycleOwner, ::changeProgress)
     }
     
     override fun setData(block: GraphBlock) {
         if (block !is ControllerBlock) return
         
         viewModel.onNewBlockData(block)
+    }
+    
+    private fun bind(controller: Controller) {
+        controller_name.text = controller.name
+        controller_state.text = controller.state
+    }
+    
+    private fun changeProgress(isLoading: Boolean) {
+        controller_progress.visible = isLoading
     }
     
     private fun moveTo(position: Position) {
@@ -78,28 +84,3 @@ class GraphControllerView @JvmOverloads constructor(
     }
 }
 
-class GraphControllerViewModel: KoinViewModel() {
-    private val eventBus: GraphEventBus by inject()
-    private var id: Long? = null
-    val visible = MutableLiveData<Boolean>()
-    val position = MutableLiveData<Position>()
-    
-    fun onNewBlockData(block: ControllerBlock) {
-        // todo on first init observe controller
-        id = block.id.id
-        visible.value = block.visible
-        position.value = block.position
-        
-        // add live data for state
-    }
-    
-    fun onDragStarted(dragTouch: Position): ControllerDragEvent? {
-        return id?.let { id ->
-            ControllerDragEvent(id = id, dragInfo = CommonDragInfo(
-                status = DRAG_START,
-                dragTouch = dragTouch,
-                from = GRAPH
-            )).also(eventBus::addEvent)
-        }
-    }
-}
