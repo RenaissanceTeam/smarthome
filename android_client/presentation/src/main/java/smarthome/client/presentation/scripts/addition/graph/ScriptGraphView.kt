0f3cff6cc6dc697d69eec65cbad7eab5b3052ef5
Nikolay Skudniakov <helloma@yandex.ru>
2020-02-05 07:45:12 +0300

    Refactoring to remove compilation errors

--- android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/ScriptGraphView.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/addition/graph/ScriptGraphView.kt
@@ -10,17 +10,17 @@ import androidx.lifecycle.observe
 import kotlinx.android.synthetic.main.scripts_graph.view.*
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+import smarthome.client.entity.script.BlockId
+import smarthome.client.entity.script.Position
+import smarthome.client.entity.script.toPosition
 import smarthome.client.presentation.R
 import smarthome.client.presentation.scripts.addition.graph.blockviews.GraphBlockView
 import smarthome.client.presentation.scripts.addition.graph.blockviews.dependency.*
 import smarthome.client.presentation.scripts.addition.graph.blockviews.factory.GraphBlockFactoryResolver
 import smarthome.client.presentation.scripts.addition.graph.blockviews.state.BlockState
 import smarthome.client.presentation.scripts.addition.graph.events.drag.GraphDragEvent
-import smarthome.client.presentation.scripts.addition.graph.identifier.GraphBlockIdentifier
-import smarthome.client.presentation.util.Position
 import smarthome.client.presentation.util.inflate
 import smarthome.client.presentation.util.lifecycleOwner
-import smarthome.client.entity.script.toPosition
 
 class ScriptGraphView @JvmOverloads constructor(
     context: Context,
@@ -32,7 +32,7 @@ class ScriptGraphView @JvmOverloads constructor(
         inflate(R.layout.scripts_graph)
     }
     
-    private var blockViews = mutableMapOf<GraphBlockIdentifier, GraphBlockView>()
+    private var blockViews = mutableMapOf<BlockId, GraphBlockView>()
     private var dependencyViews = mutableMapOf<String, DependencyArrowView>()
     private val viewModel = ScriptGraphViewModel()
     private val graphBlockFactoryResolver: GraphBlockFactoryResolver by inject()
@@ -57,34 +57,34 @@ class ScriptGraphView @JvmOverloads constructor(
                     val movedTo = findBlockOnDependencyTip(tip.rawPosition)
                 
                     when (movedTo == null) {
-                        false -> viewModel.dependencyTipOnBlock(tip.from, movedTo.key)
+                        false -> tip.from?.let { viewModel.dependencyTipOnBlock(it, movedTo.key) }
                         true -> viewModel.dependencyTipNotOnAnyBlock()
                     }
                 }
                 DROPPED -> {
                     val droppedTo = findBlockOnDependencyTip(tip.rawPosition)
-                
+                    if (tip.dependencyId == null) return@observe
                     when (droppedTo == null) {
-                        false -> tip.dependencyId?.let {
-                            viewModel.addDependency(it, tip.from, droppedTo.key)
+                        false -> if (tip.from != null) {
+                            viewModel.addDependency(tip.dependencyId, tip.from, droppedTo.key)
                         }
-                        true -> tip.dependencyId?.let { viewModel.cancelCreatingDependency(it) }
+                        true -> viewModel.cancelCreatingDependency(tip.dependencyId)
                     }
                 }
             }
         }
     }
     
-    private fun bindBlocks(blocks: MutableMap<GraphBlockIdentifier, BlockState>) {
+    private fun bindBlocks(blocks: List<BlockState>) {
         retainOnlyPostedBlocks(blocks)
-
-        blocks.values.forEach { block ->
+        
+        blocks.forEach { block ->
             getOrInflateBlockView(block).setData(block)
         }
     }
     
     private fun findBlockOnDependencyTip(
-        rawPosition: Position): Map.Entry<GraphBlockIdentifier, GraphBlockView>? {
+        rawPosition: Position): Map.Entry<BlockId, GraphBlockView>? {
         return blockViews.asIterable().find { entry ->
             val block = entry.value
             
@@ -93,8 +93,8 @@ class ScriptGraphView @JvmOverloads constructor(
         }
     }
     
-    private fun retainOnlyPostedBlocks(blocks: MutableMap<GraphBlockIdentifier, BlockState>) {
-        (blockViews.keys - blocks.keys).forEach {
+    private fun retainOnlyPostedBlocks(blocks: List<BlockState>) {
+        (blockViews.keys - blocks.map { it.block.id }).forEach {
             (blockViews.remove(it) as? View)?.let(this::removeView)
         }
     }
@@ -106,12 +106,12 @@ class ScriptGraphView @JvmOverloads constructor(
     }
     
     private fun getOrInflateBlockView(blockState: BlockState): GraphBlockView {
-        var blockView = blockViews[blockState.id]
+        var blockView = blockViews[blockState.block.id]
         
         if (blockView == null) {
             val viewFactory = graphBlockFactoryResolver.resolve(blockState)
             blockView = viewFactory.inflate(graph, blockState)
-            blockViews[blockState.id] = blockView
+            blockViews[blockState.block.id] = blockView
         }
         
         return blockView
@@ -160,7 +160,7 @@ class ScriptGraphView @JvmOverloads constructor(
                     true
                 }
                 DragEvent.ACTION_DROP -> {
-                    viewModel.onDropped(dragInfo, Position(event.x, event.y))
+                    viewModel.onDropped(dragInfo, Position(event.x.toInt(), event.y.toInt()))
                     true
                 }
                 else -> false
--- android_client/presentation/src/test/java/smarthome/client/presentation/scripts/addition/graph/MockBlockState.kt
+++ android_client/presentation/src/test/java/smarthome/client/presentation/scripts/addition/graph/MockBlockState.kt
@@ -1,19 +1,18 @@
 package smarthome.client.presentation.scripts.addition.graph
 
-import smarthome.client.presentation.scripts.addition.graph.blockviews.state.BorderStatus
+import smarthome.client.entity.script.Block
 import smarthome.client.presentation.scripts.addition.graph.blockviews.state.BlockState
-import smarthome.client.presentation.util.Position
+import smarthome.client.presentation.scripts.addition.graph.blockviews.state.BorderStatus
 
 data class MockBlockState(
-    override val id: MockBlockId,
-    override val position: Position,
+    override val block: Block,
     override val visible: Boolean = true,
     override val border: BorderStatus = BorderStatus()
 ) : BlockState {
     
-    override fun copyWithInfo(position: Position, visible: Boolean,
+    override fun copyWithInfo(block: Block, visible: Boolean,
                               border: BorderStatus): BlockState {
-        return copy(position = position, visible = visible,
+        return copy(block = block, visible = visible,
             border = border)
     }
     
