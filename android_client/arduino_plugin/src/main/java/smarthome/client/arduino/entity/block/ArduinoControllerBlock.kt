8924dec115fa10d459393efe59b54b518b620667
Nikolay Skudniakov <helloma@yandex.ru>
2020-02-07 23:08:36 +0300

    Fixing issues after refactoring

--- android_client/arduino_plugin/src/main/java/smarthome/client/arduino/entity/block/ArduinoControllerBlock.kt
+++ android_client/arduino_plugin/src/main/java/smarthome/client/arduino/entity/block/ArduinoControllerBlock.kt
@@ -1,16 +1,17 @@
 package smarthome.client.arduino.entity.block
 
 import smarthome.client.entity.script.block.Block
-import smarthome.client.entity.script.block.BlockId
+import smarthome.client.entity.script.controller.ControllerBlock
+import smarthome.client.entity.script.controller.ControllerBlockId
 import smarthome.client.util.Position
 
-data class ControllerBlock(
+data class ArduinoControllerBlock(
     val controllerId: Long,
     val type: String,
     override val position: Position
-) : Block {
+) : ControllerBlock {
     
-    override val id: BlockId = ControllerBlockId(controllerId)
+    override val id = ControllerBlockId(controllerId)
     
     override fun copyWithPosition(newPosition: Position): Block {
         return copy(position = newPosition)
--- android_client/arduino_plugin/src/main/java/smarthome/client/arduino/entity/block/ControllerBlockId.kt
+++
@@ -1,5 +0,0 @@
-package smarthome.client.arduino.entity.block
-
-import smarthome.client.entity.script.block.BlockId
-
-data class ControllerBlockId(val id: Long): BlockId
