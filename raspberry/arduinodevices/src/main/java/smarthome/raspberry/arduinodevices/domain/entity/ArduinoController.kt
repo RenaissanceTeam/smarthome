fb03ef12e52af97af8500f725e6ec04c27f0b165
Nikolay Skudniakov <helloma@yandex.ru>
2020-04-06 23:13:37 +0300

    fix entity, update upload script

--- raspberry/arduinodevices/src/main/java/smarthome/raspberry/arduinodevices/domain/entity/ArduinoController.kt
+++ raspberry/arduinodevices/src/main/java/smarthome/raspberry/arduinodevices/domain/entity/ArduinoController.kt
@@ -5,12 +5,14 @@ import smarthome.raspberry.entity.controller.Controller
 import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.Id
+import javax.persistence.OneToOne
 
 @Entity
 data class ArduinoController(
 
         @Id @GeneratedValue
         val id: Long = ID_NOT_DEFINED,
+        @OneToOne
         val controller: Controller,
         val serial: Int
 )
