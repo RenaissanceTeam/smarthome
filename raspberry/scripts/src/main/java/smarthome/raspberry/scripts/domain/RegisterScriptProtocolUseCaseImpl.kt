c801f61395b1ba6642b10694d58a4cf337dccb47
Nikolay Skudniakov <helloma@yandex.ru>
2020-04-22 00:40:07 +0300

    implement triggering actions on validated conditons

--- raspberry/scripts/src/main/java/smarthome/raspberry/scripts/domain/RegisterScriptProtocolUseCaseImpl.kt
+++ raspberry/scripts/src/main/java/smarthome/raspberry/scripts/domain/RegisterScriptProtocolUseCaseImpl.kt
@@ -3,14 +3,11 @@ package smarthome.raspberry.scripts.domain
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.disposables.Disposable
 import org.springframework.stereotype.Component
-import smarthome.raspberry.entity.script.Block
-import smarthome.raspberry.entity.script.Condition
-import smarthome.raspberry.entity.script.Dependency
-import smarthome.raspberry.entity.script.Script
+import smarthome.raspberry.entity.script.*
+import smarthome.raspberry.scripts.api.domain.ActionRunner
 import smarthome.raspberry.scripts.api.domain.BlockObserver
 import smarthome.raspberry.scripts.api.domain.ConditionValidator
 import smarthome.raspberry.scripts.api.domain.RegisterScriptProtocolUseCase
-import smarthome.raspberry.scripts.api.domain.RunScriptActionUseCase
 import javax.transaction.Transactional
 
 @Component
@@ -18,8 +15,7 @@ import javax.transaction.Transactional
 open class RegisterScriptProtocolUseCaseImpl(
         private val blockObservers: Map<String, BlockObserver>,
         private val conditionValidators: Map<String, ConditionValidator>,
-        private val runScriptActionUseCase: RunScriptActionUseCase
-
+        private val actionRunners: Map<String, ActionRunner>
 ) : RegisterScriptProtocolUseCase {
     private val disposable = CompositeDisposable()
 
@@ -29,10 +25,17 @@ open class RegisterScriptProtocolUseCaseImpl(
         topDependencies.map { dependency ->
             val validators = dependency.conditions.map { condition ->
                 val validator = conditionValidators[composeValidatorName(condition)]
-                        ?: throw IllegalStateException("No condition validator for $dependency")
+                        ?: throw IllegalStateException("No condition validator for $condition")
 
                 condition to validator
             }
+
+            val runners = dependency.actions.map { action ->
+                val runner = actionRunners[composeRunnerName(action)]
+                        ?: throw IllegalStateException("No action runner for $action")
+
+                action to runner
+            }.toMap()
 
             val blockObserver = blockObservers[composeObserverName(dependency.start)]
                     ?: throw IllegalStateException("No block observer for ${dependency.start}")
@@ -40,7 +43,12 @@ open class RegisterScriptProtocolUseCaseImpl(
             dependency.start.id.let(blockObserver::execute)
                     .map { block -> validators.all { it.second.validate(it.first, block) } }
                     .distinct()
-                    .doOnNext { if (it) dependency.actions.forEach { runScriptActionUseCase.execute(dependency.end, it) } }
+                    .filter { it }
+                    .doOnNext {
+                        dependency.actions.forEach { action ->
+                            runners[action]?.runAction(action, dependency.end.id)
+                        }
+                    }
         }.map { it.subscribe() }.forEach { disposable.add(it) }
 
         return disposable
@@ -48,6 +56,7 @@ open class RegisterScriptProtocolUseCaseImpl(
 
     private fun composeObserverName(block: Block) = "${block::class.simpleName!!.decapitalize()}Observer"
     private fun composeValidatorName(condition: Condition) = "${condition::class.simpleName!!.decapitalize()}Validator"
+    private fun composeRunnerName(action: Action) = "${action::class.simpleName!!.decapitalize()}Runner"
 
     private fun findTopDependencies(script: Script): List<Dependency> {
         val topBlocks = script.blocks.filter { !script.dependencies.map { it.end.id }.contains(it.id) }
