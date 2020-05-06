73fa362b621cd89d1a60a7c09a2aca224d3f5e21
Nikolay Skudniakov <helloma@yandex.ru>
2020-05-06 17:36:21 +0300

    change api of dependency arrow

--- android_client/presentation/src/main/java/smarthome/client/presentation/scripts/setup/graph/blockviews/dependency/DependencyArrowView.kt
+++ android_client/presentation/src/main/java/smarthome/client/presentation/scripts/setup/graph/blockviews/dependency/DependencyArrowView.kt
@@ -12,49 +12,61 @@ import smarthome.client.util.emptyPosition
 import kotlin.properties.Delegates
 
 class DependencyArrowView @JvmOverloads constructor(
-    context: Context,
-    attrs: AttributeSet? = null,
-    defStyleAttr: Int = 0
+        context: Context,
+        attrs: AttributeSet? = null,
+        defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
     private val paint = Paint()
+
     init {
         paint.color = Color.RED
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = 7f
         paint.isAntiAlias = true
-    
+
         inflate(context, R.layout.scripts_dependency_arrow, null)
     }
-    
+
     private var startPosition: Position by Delegates.observable(emptyPosition) { _, old, new ->
         if (old == new) return@observable
-        
+
         invalidate()
     }
-    
+
     private var endPosition: Position by Delegates.observable(emptyPosition) { _, old, new ->
         if (old == new) return@observable
-        
+
         invalidate()
     }
-    
-    fun setStart(position: Position) {
-        startPosition = position
+
+
+    fun setPositions(start: DependencyTip, end: DependencyTip) {
+        startPosition = start.center
+        endPosition = end.center
     }
-    
-    fun setEnd(position: Position) {
-        endPosition = position
-    }
-    
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         canvas.drawLine(
-            startPosition.x.toFloat(),
-            startPosition.y.toFloat(),
-            endPosition.x.toFloat(),
-            endPosition.y.toFloat(),
-            paint
+                startPosition.x.toFloat(),
+                startPosition.y.toFloat(),
+                endPosition.x.toFloat(),
+                endPosition.y.toFloat(),
+                paint
         )
     }
 }
 
+data class DependencyTip(
+        val position: Position,
+        val width: Int,
+        val height: Int
+) {
+    val center = position + Position(width / 2, height / 2)
+}
+//
+//class BorderIntersectionHelper {
+//    fun findIntersection(blockPosition: Position, width: Int, height: Int, dependencyEnd: Position): Position {
+//
+//    }
+//}
