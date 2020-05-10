f61778498dbf49f7a03b658e98e6bd06f4d36f22
Nikolay Skudniakov <helloma@yandex.ru>
2020-05-10 15:49:19 +0300

    add scripts to get all programming code info

---
+++ android_client/app/src/main/java/smarthome/client/AppStatistics.kt
@@ -0,0 +1,75 @@
+package smarthome.client
+
+import android.app.Application
+import android.os.Looper
+import java.io.File
+import java.lang.reflect.Modifier
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.streams.toList
+
+fun main() {
+    val classes = findAllClasses()
+    val allFiles = findAllFiles()
+
+
+
+    File("classInfo.out").printWriter().use { output ->
+        allFiles
+                .map { file ->
+                    val filename = file.name.split(".")[0]
+                    val c = classes.keys.find { it.simpleName == filename } ?: return@map null
+
+                    c to file
+                }
+                .filterNotNull()
+                .forEachIndexed { index, it ->
+                    output.println("_#subkey")
+                    output.println(getTitle(it.first))
+                    output.println()
+                    output.println(it.second.readText())
+                    output.println()
+                    output.println()
+                }
+    }
+}
+
+private fun findAllClasses(): Map<Class<Any>, ClassInfo> {
+    val classes = JavaFindClassHelper.getClasses("smarthome")
+            .map { it to ClassInfo(getTitle(it)) }
+            .toMap()
+    return classes
+}
+
+private fun findAllFiles(): List<File> {
+    val allFiles = Files.walk(Paths.get("."))
+            .filter {
+                Files.isRegularFile(it)
+            }
+            .map { it.toFile() }
+            .filter { !it.path.contains("/out/") }
+            .filter { !it.path.contains("/build/") }
+            .filter { !it.path.contains("/thirdpartydevices/") }
+            .filter { !it.name.contains("Test.kt") }
+            .filter { it.name.endsWith("kt") }
+            .toList()
+    return allFiles
+}
+
+data class ClassInfo(
+        val title: String,
+        val sourceCode: String = ""
+)
+
+private fun getTitle(c: Class<Any>): String {
+    var result = ""
+    if (c.isInterface) {
+        result += "Интерфейс "
+    } else {
+        if (Modifier.isAbstract(c.modifiers)) result += "Абстрактный "
+        result += "Класс "
+    }
+    result += c.simpleName
+
+    return result
+}
--- raspberry/devices_api/src/main/java/smarthome/raspberry/devices/api/domain/GetDeviceByControllerUseCase.kt
+++
@@ -1,8 +0,0 @@
-package smarthome.raspberry.devices.api.domain
-
-import smarthome.raspberry.entity.controller.Controller
-import smarthome.raspberry.entity.device.Device
-
-interface GetDeviceByControllerUseCase {
-    suspend fun execute(controller: Controller): Device
-}
