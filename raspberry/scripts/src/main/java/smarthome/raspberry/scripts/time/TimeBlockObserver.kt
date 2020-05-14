1ccb19f5c5f0796d213dd664608fb79f7eb3f388
Nikolay Skudniakov <helloma@yandex.ru>
2020-05-14 21:23:08 +0300

    refactoring garbage

--- raspberry/scripts/src/main/java/smarthome/raspberry/scripts/time/TimeBlockObserver.kt
+++ raspberry/scripts/src/main/java/smarthome/raspberry/scripts/time/TimeBlockObserver.kt
@@ -3,45 +3,38 @@ package smarthome.raspberry.scripts.time
 import io.reactivex.Observable
 import org.joda.time.LocalTime
 import org.springframework.stereotype.Component
-import smarthome.raspberry.entity.script.Block
 import smarthome.raspberry.scripts.api.domain.BlockObserver
-import smarthome.raspberry.scripts.api.domain.usecase.GetBlockByIdUseCase
 import smarthome.raspberry.scripts.api.domain.usecase.GetBlockConditionDependenciesUseCase
-import smarthome.raspberry.scripts.api.time.*
+import smarthome.raspberry.scripts.api.time.EachDayStateValid
+import smarthome.raspberry.scripts.api.time.TimeStateValid
+import smarthome.raspberry.scripts.api.time.TimerStateValid
+import smarthome.raspberry.scripts.api.time.WeekdayStateValid
 import smarthome.raspberry.scripts.api.time.conditions.EachDayCondition
 import smarthome.raspberry.scripts.api.time.conditions.TimerCondition
 import smarthome.raspberry.scripts.api.time.conditions.WeekdaysCondition
 import java.util.*
 import java.util.concurrent.TimeUnit
 
+private data class Period(
+        val delay: Long,
+        val amount: Long,
+        val unit: TimeUnit
+)
+
+private val LocalTime.millis get() = this.toDateTimeToday().millis
+
 @Component
 class TimeBlockObserver(
-        private val getBlockConditionDependenciesUseCase: GetBlockConditionDependenciesUseCase,
-        private val getBlockByIdUseCase: GetBlockByIdUseCase
-
+        private val getBlockConditionDependenciesUseCase: GetBlockConditionDependenciesUseCase
 ) : BlockObserver<TimeStateValid> {
     override fun execute(blockId: String): Observable<Optional<TimeStateValid>> {
         return getBlockConditionDependenciesUseCase.execute(blockId)
                 .flatMap { it.conditions }
                 .map { condition ->
                     when (condition) {
-                        is TimerCondition -> schedulePeriod(
-                                Period(condition.timer.toLong(), condition.timer.toLong(), TimeUnit.SECONDS)
-                        ).map { TimerStateValid(condition) }
-                        is EachDayCondition -> schedulePeriod(
-                                Period(calculateDelay(Date(condition.time.millis)), 1, TimeUnit.DAYS)
-                        ).map { EachDayStateValid(condition) }
-                        is WeekdaysCondition -> {
-                            Observable.merge(
-                                    condition.days.map {
-                                        val firstEmit = getStartOfWeek() + it * condition.time.millis
-                                        schedulePeriod(
-                                                Period(calculateDelay(Date(firstEmit)), 7, TimeUnit.DAYS)
-                                        ).filter { it > System.currentTimeMillis() }
-                                    })
-                                    .map { WeekdayStateValid(condition) }
-
-                        }
+                        is TimerCondition -> scheduleTimer(condition)
+                        is EachDayCondition -> scheduleEachDay(condition)
+                        is WeekdaysCondition -> scheduleWeekdays(condition)
                         else -> throw IllegalArgumentException("Unknown condition for time block: $condition")
                     }
 
@@ -51,9 +44,30 @@ class TimeBlockObserver(
                 .startWith(Observable.just(Optional.empty()))
     }
 
-    private val LocalTime.millis get() = this.toDateTimeToday().millis
+    private fun scheduleWeekdays(condition: WeekdaysCondition): Observable<WeekdayStateValid>? {
+        return Observable.merge(
+                condition.days.map {
+                    val firstEmit = getStartOfWeek() + it * condition.time.millis
+                    schedulePeriod(
+                            Period(calculateDelay(Date(firstEmit)), 7, TimeUnit.DAYS)
+                    ).filter { it > System.currentTimeMillis() }
+                })
+                .map { WeekdayStateValid(condition) }
+    }
 
-    fun getStartOfWeek(): Long {
+    private fun scheduleEachDay(condition: EachDayCondition): Observable<EachDayStateValid>? {
+        return schedulePeriod(
+                Period(calculateDelay(Date(condition.time.millis)), 1, TimeUnit.DAYS)
+        ).map { EachDayStateValid(condition) }
+    }
+
+    private fun scheduleTimer(condition: TimerCondition): Observable<TimerStateValid>? {
+        return schedulePeriod(
+                Period(condition.timer.toLong(), condition.timer.toLong(), TimeUnit.SECONDS)
+        ).map { TimerStateValid(condition) }
+    }
+
+    private fun getStartOfWeek(): Long {
         return Calendar.getInstance().apply {
             time = Date(System.currentTimeMillis())
             set(Calendar.DAY_OF_WEEK, Calendar.MONDAY)
@@ -73,17 +87,4 @@ class TimeBlockObserver(
         return Observable.interval(period.delay, period.amount, period.unit)
                 .map { System.currentTimeMillis() }
     }
-
-
-    private fun mapTimeToBlockState(blockId: String, time: Long): Optional<Block> {
-        return (getBlockByIdUseCase.execute(blockId) as? TimeBlock)
-                ?.copy(time = LocalTime(time))
-                .let { Optional.ofNullable(it) }
-    }
-
-    data class Period(
-            val delay: Long,
-            val amount: Long,
-            val unit: TimeUnit
-    )
 }
--- raspberry/scripts_api/src/main/java/smarthome/raspberry/scripts/api/time/data/TimeBlockStateRepository.kt
+++
@@ -1,9 +0,0 @@
-package smarthome.raspberry.scripts.api.time.data
-
-import io.reactivex.Observable
-import org.joda.time.LocalTime
-
-
-interface TimeBlockStateRepository {
-
-}
